2c08517cd9acb94109755162e369fdc8c05349f4
znerd <>
2003-03-20 16:58:05 +0000

    Added _namesToValues and _valuesToNames.

--- src/java-server-framework/org/xins/types/EnumType.java
+++ src/java-server-framework/org/xins/types/EnumType.java
@@ -1,4 +1,4 @@
-/*
+/* Ernst is een enorme flapdrolllllll!!!!!!
  * $Id$
  */
 package org.xins.types;
@@ -44,6 +44,9 @@ public abstract class EnumType extends Type {
     * <code>items == null</code>, then this type will contain no items. This
     * is the same as passing a zero-size {@link EnumItem} array.
     *
+    * <p />Note that the <code>items</code> array may contain
+    * <code>null</code> values. These will be ignored.
+    *
     * @param name
     *    the name of the type, not <code>null</code>.
     *
@@ -56,12 +59,38 @@ public abstract class EnumType extends Type {
    protected EnumType(String name, EnumItem[] items)
    throws IllegalArgumentException {
       super(name, String.class);
+
+      _namesToValues = new HashMap();
+      _valuesToNames = new HashMap();
+
+      int count = items == null ? 0 : items.length;
+      for (int i = 0; i < count; i++) {
+         EnumItem item = items[i];
+         if (item != null) {
+            String itemName  = item.getName();
+            String itemValue = item.getValue();
+
+            _namesToValues.put(itemName,  itemValue);
+            _valuesToNames.put(itemValue, itemName);
+         }
+      }
    }
 
 
    //-------------------------------------------------------------------------
    // Fields
    //-------------------------------------------------------------------------
+
+   /**
+    * Map that links symbolic names to enumeration values.
+    */
+   private final Map _namesToValues;
+
+   /**
+    * Map that links enumeration values to their symbolic names.
+    */
+   private final Map _valuesToNames;
+
 
    //-------------------------------------------------------------------------
    // Methods
