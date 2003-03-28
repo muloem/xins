1c551bae9448a50cc788df6c9dfb88db0f72be1c
znerd <>
2003-03-28 08:14:42 +0000

    Fixed bug.

--- src/bin/xins.bat
+++ src/bin/xins.bat
@@ -28,7 +28,7 @@ IF NOT EXIST build\NUL GOTO builddir_existent
 MKDIR build
 
 :builddir_existent
-CALL ant -f %XINS_HOME\src\ant\transform.xml -Din=xins-project.xml -Dout=build\build.xml -Dstyle=%XINS_HOME%\src\xslt\xins-project_to_ant-build.xslt -Dxins_home=%XINS_HOME% -Dproject_home=. -Dbuilddir=build
+CALL ant -f %XINS_HOME%\src\ant\transform.xml -Din=xins-project.xml -Dout=build\build.xml -Dstyle=%XINS_HOME%\src\xslt\xins-project_to_ant-build.xslt -Dxins_home=%XINS_HOME% -Dproject_home=. -Dbuilddir=build
 
 CALL ant -f build\build.xml %1 %2 %3 %4 %5 %6 %7 %8 %9
 :end
