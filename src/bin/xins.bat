a212a5d87a1f79d5addf3e7dfa3b1b4a3ddcb02a
znerd <>
2003-03-31 08:52:51 +0000

    Determining project home directory.

--- src/bin/xins.bat
+++ src/bin/xins.bat
@@ -16,6 +16,16 @@ CALL ant -version
 CALL ant -f %XINS_HOME%\build.xml -listener org.apache.tools.ant.NoBannerLogger version
 GOTO end
 
+:: Determine the current drive and directory
+ECHO @PROMPT SET CURDRV=$N:$_SET CURDIR=$P> %temp%.\drvdir01.bat
+%COMSPEC% /E:2048 /C%temp%.\drvdir01.bat > %temp%.\drvdir02.bat
+CALL %temp%.\drvdir02.bat
+DEL %temp%.\drvdir01.bat
+DEL %temp%.\drvdir02.bat
+IF EXIST %CURDIR%\nul SET CURDIR=%CURDIR%\
+ECHO. Current drive is %CURDRV%
+ECHO. Current directory is %CURDIR%
+
 :: Make sure XINS_HOME is set
 :no_version
 IF NOT "%XINS_HOME%" == "" GOTO xins_home_set
@@ -37,7 +47,7 @@ IF NOT EXIST build\NUL GOTO builddir_existent
 MKDIR build
 
 :builddir_existent
-CALL ant -f %XINS_HOME%\src\ant\transform.xml -Din=xins-project.xml -Dout=build\build.xml -Dstyle=%XINS_HOME%\src\xslt\xins-project_to_ant-build.xslt -Dxins_home=%XINS_HOME% -Dproject_home=. -Dbuilddir=build
+CALL ant -f %XINS_HOME%\src\ant\transform.xml -Din=%CURDRIVE%:\%CURDIR%xins-project.xml -Dout=%CURDRIVE%:\%CURDIR%build\build.xml -Dstyle=%XINS_HOME%\src\xslt\xins-project_to_ant-build.xslt -Dxins_home=%XINS_HOME% -Dproject_home=%CURDRV%:\%CURDIR% -Dbuilddir=build
 
-CALL ant -f build\build.xml %1 %2 %3 %4 %5 %6 %7 %8 %9
+CALL ant -f %CURDRV%:\%CURDIR%build\build.xml %1 %2 %3 %4 %5 %6 %7 %8 %9
 :end
