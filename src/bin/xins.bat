334feabb6ec1c615c1ccada8b79653abfec931db
znerd <>
2003-03-31 10:18:47 +0000

    Properly detecting current directory. This version has actually been tested to work on Windows 2000.

--- src/bin/xins.bat
+++ src/bin/xins.bat
@@ -5,6 +5,11 @@
 ::
 :: Converts xins-project.xml in the current directory to an Ant build file and
 :: then passes control to Ant.
+::
+:: This batch file is intended to be used on Windows 2000 and higher. It has
+:: been tested on:
+::
+::     * Windows 2000 SP 3 (build 5.00.2195)
 
 @ECHO OFF
 
@@ -13,28 +18,26 @@
 IF NOT "%1" == "-version" GOTO no_version
 java -version
 CALL ant -version
-CALL ant -f %XINS_HOME%\build.xml -listener org.apache.tools.ant.NoBannerLogger version
+CALL ant -f %XINS_HOME%\build.xml -listener org.apache.tools.ant.NoBannerLogger
+version
 GOTO end
 
 :: Determine the current drive and directory
-ECHO @PROMPT SET CURDRV=$N:$_SET CURDIR=$P> %temp%.\drvdir01.bat
-%COMSPEC% /E:2048 /C%temp%.\drvdir01.bat > %temp%.\drvdir02.bat
-CALL %temp%.\drvdir02.bat
-DEL %temp%.\drvdir01.bat
-DEL %temp%.\drvdir02.bat
-IF EXIST %CURDIR%\nul SET CURDIR=%CURDIR%\
-ECHO. Current drive is %CURDRV%
-ECHO. Current directory is %CURDIR%
+:no_version
+SETLOCAL
+SET project_home=
+FOR /f "Tokens=*" %%i IN ('CD') DO SET project_home=%%i
+ECHO Current directory is %project_home%
 
 :: Make sure XINS_HOME is set
-:no_version
 IF NOT "%XINS_HOME%" == "" GOTO xins_home_set
 ECHO FATAL: XINS_HOME not set
 GOTO end
 
 :xins_home_set
 IF EXIST %XINS_HOME%\src\xslt\xins-project_to_ant-build.xslt GOTO stylesheet_found
-ECHO FATAL: Cannot find stylesheet %XINS_HOME%\src\xslt\xins-project_to_ant-build.xslt
+ECHO FATAL: Cannot find stylesheet
+%XINS_HOME%\src\xslt\xins-project_to_ant-build.xslt
 GOTO end
 
 :stylesheet_found
@@ -47,7 +50,7 @@ IF NOT EXIST build\NUL GOTO builddir_existent
 MKDIR build
 
 :builddir_existent
-CALL ant -f %XINS_HOME%\src\ant\transform.xml -Din=%CURDRIVE%:\%CURDIR%xins-project.xml -Dout=%CURDRIVE%:\%CURDIR%build\build.xml -Dstyle=%XINS_HOME%\src\xslt\xins-project_to_ant-build.xslt -Dxins_home=%XINS_HOME% -Dproject_home=%CURDRV%:\%CURDIR% -Dbuilddir=build
+CALL ant -f %XINS_HOME%\src\ant\transform.xml -Din=%project_home%\xins-project.xml -Dout=%project_home%\build\build.xml -Dstyle=%XINS_HOME%\src\xslt\xins-project_to_ant-build.xslt -Dxins_home=%XINS_HOME% -Dproject_home=%project_home% -Dbuilddir=build
 
-CALL ant -f %CURDRV%:\%CURDIR%build\build.xml %1 %2 %3 %4 %5 %6 %7 %8 %9
+CALL ant -f %project_home%\build\build.xml %1 %2 %3 %4 %5 %6 %7 %8 %9
 :end
