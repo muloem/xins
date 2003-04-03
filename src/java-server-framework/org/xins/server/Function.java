d19ad3f7e0b7408e1287e5ba30b0581bd9d7f9b9
znerd <>
2003-04-03 09:48:03 +0000

    Added method performedCall().

--- src/java-server-framework/org/xins/server/Function.java
+++ src/java-server-framework/org/xins/server/Function.java
@@ -56,8 +56,72 @@ extends Object {
     */
    private final API _api;
 
+   /**
+    * Lock object for a successful call.
+    */
+   private final Object _successfulCallLock = new Object();
+
+   /**
+    * Lock object for an unsuccessful call.
+    */
+   private final Object _unsuccessfulCallLock = new Object();
+
+   /**
+    * The number of successful calls executed up until now.
+    */
+   private int _successfulCalls;
+
+   /**
+    * The number of unsuccessful calls executed up until now.
+    */
+   private int _unsuccessfulCalls;
+
+   /**
+    * The total duration of all successful calls up until now.
+    */
+   private int _successfulDuration;
+
+   /**
+    * The total duration of all unsuccessful calls up until now.
+    */
+   private int _unsuccessfulDuration;
+
 
    //-------------------------------------------------------------------------
    // Methods
    //-------------------------------------------------------------------------
+
+   /**
+    * Callback method that may be called after a call to this function. This
+    * method will store statistics-related information.
+    *
+    * <p />This method does not <em>have</em> to be called. If statistics
+    * gathering is disabled, then this method should not be called.
+    *
+    * @param start
+    *    the timestamp indicating when the call was started, as a number of
+    *    milliseconds since midnight January 1, 1970 UTC.
+    *
+    * @param duration
+    *    the duration of the function call, as a number of milliseconds.
+    *
+    * @param success
+    *    indication if the call was successful.
+    *
+    * @param code
+    *    the function result code, or <code>null</code>.
+    */
+   public final void performedCall(long start, long duration, boolean success, String code) {
+      if (success) {
+         synchronized (_successfulCallLock) {
+            _successfulCalls++;
+            _successfulDuration += duration;
+         }
+      } else {
+         synchronized (_unsuccessfulCallLock) {
+            _unsuccessfulCalls++;
+            _unsuccessfulDuration += duration;
+         }
+      }
+   }
 }
