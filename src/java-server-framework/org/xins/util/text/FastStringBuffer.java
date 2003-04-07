03acbeceeaff8ab00377bd29e0630693e792e8a4
znerd <>
2003-04-07 10:51:13 +0000

    Fixed email address.

--- src/java-server-framework/org/xins/util/text/FastStringBuffer.java
+++ src/java-server-framework/org/xins/util/text/FastStringBuffer.java
@@ -9,7 +9,7 @@ import org.xins.util.MandatoryArgumentChecker;
  * Fast, unsynchronized string buffer implementation.
  *
  * @version $Revision$ $Date$
- * @author Ernst de Haan (<a href="mailto:ernst.dehaan@nl.wanadoo.com">ernst.dehaan@nl.wanadoo.com</a>)
+ * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
  */
 public class FastStringBuffer extends Object {
 
