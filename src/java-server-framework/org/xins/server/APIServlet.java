bb9391b7b285b6628019d26fa64a89a33a7deb82
znerd <>
2003-04-08 07:28:56 +0000

    Call super.init(config).

--- src/java-server-framework/org/xins/server/APIServlet.java
+++ src/java-server-framework/org/xins/server/APIServlet.java
@@ -66,6 +66,11 @@ extends HttpServlet {
 
    public void init(ServletConfig config)
    throws ServletException {
+
+      // Call the superclass, in accordance with the Servlet API spec, see:
+      // http://java.sun.com/products/servlet/2.3/javadoc/javax/servlet/GenericServlet.html#init(javax.servlet.ServletConfig)
+      super.init(config);
+
       String apiClass = config.getInitParameter("api.class");
       if (apiClass == null || apiClass.equals("")) {
          throw new ServletException("Unable to initialize servlet \"" + config.getServletName() + "\", API class should be set in init parameter \"api.class\".");
