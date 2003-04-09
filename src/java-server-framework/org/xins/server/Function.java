37a5dea990f4594d7cfb46725ee6aff309ba0d16
znerd <>
2003-04-09 16:04:57 +0000

    Added getReturnCodes().

--- src/java-server-framework/org/xins/server/Function.java
+++ src/java-server-framework/org/xins/server/Function.java
@@ -197,6 +197,19 @@ extends Object {
    }
 
    /**
+    * Gets a list of the defined return codes. Default return (not specific to
+    * any API or function) codes are not returned in this list.
+    *
+    * @return
+    *    the defined return codes as a {@link List} containing only
+    *    {@link String} instances, can be <code>null</code> if no return codes
+    *    are defined.
+    */
+   final List getReturnCodes() {
+      return null; // TODO
+   }
+
+   /**
     * Returns the call statistics for this function.
     *
     * @return
