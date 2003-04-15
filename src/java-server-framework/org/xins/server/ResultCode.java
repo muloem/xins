e5e29dd2cdf2a235ac91c302eb87d6c240e452c2
znerd <>
2003-04-15 10:57:55 +0000

    Made class public.

--- src/java-server-framework/org/xins/server/ResultCode.java
+++ src/java-server-framework/org/xins/server/ResultCode.java
@@ -14,7 +14,7 @@ import org.xins.util.MandatoryArgumentChecker;
  * @version $Revision$ $Date$
  * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
  */
-final class ResultCode
+public final class ResultCode
 extends Object {
 
    //-------------------------------------------------------------------------
