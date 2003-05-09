2fba78717c578b3b8b406cd9a61be132870ce488
znerd <>
2003-05-09 07:46:47 +0000

    Changed state names to match field names.

--- src/java-server-framework/org/xins/server/ResponderStates.java
+++ src/java-server-framework/org/xins/server/ResponderStates.java
@@ -15,39 +15,39 @@ public interface ResponderStates {
    /**
     * Uninitialized state. In this state no output can be written.
     */
-   static final ResponderState UNINITIALIZED = new ResponderState("uninitialized");
+   static final ResponderState UNINITIALIZED = new ResponderState("UNINITIALIZED");
 
    /**
     * Initial state, before response output is started.
     */
-   static final ResponderState BEFORE_START = new ResponderState("before start");
+   static final ResponderState BEFORE_START = new ResponderState("BEFORE_START");
 
    /**
     * State active when the output parameters can be written. This states
     * comes after {@link #BEFORE_START}.
     */
-   static final ResponderState WITHIN_PARAMS = new ResponderState("within params");
+   static final ResponderState WITHIN_PARAMS = new ResponderState("WITHIN_PARAMS");
 
    /**
     * State within the data section when a start tag has been opened, but not
     * closed yet. This state comes after {@link #WITHIN_PARAMS}.
     */
-   static final ResponderState START_TAG_OPEN = new ResponderState("start tag open");
+   static final ResponderState START_TAG_OPEN = new ResponderState("START_TAG_OPEN");
 
    /**
     * State within the data section after a start tag has been closed, but the
     * root element has not been closed yet. This state comes after
     * {@link #START_TAG_OPEN}.
     */
-   static final ResponderState WITHIN_ELEMENT = new ResponderState("within element");
+   static final ResponderState WITHIN_ELEMENT = new ResponderState("WITHIN_ELEMENT");
 
    /**
     * Final state, after response output is finished.
     */
-   static final ResponderState AFTER_END = new ResponderState("after end");
+   static final ResponderState AFTER_END = new ResponderState("AFTER_END");
 
    /**
     * Error state. Entered if an exception is thrown within an output method.
     */
-   static final ResponderState ERROR = new ResponderState("error");
+   static final ResponderState ERROR = new ResponderState("ERROR");
 }
