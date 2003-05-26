bd0b203f946573bf8b53b68017e366fa70327280
znerd <>
2003-05-26 14:45:59 +0000

    Initial version.

---
+++ src/java-common/org/xins/util/collections/expiry/ExpiryStrategy.java
@@ -0,0 +1,64 @@
+/*
+ * $Id$
+ */
+package org.xins.util.collections.expiry;
+
+/**
+ * Expiry strategy. A strategy maintains a time-out and a time-out precision.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
+ */
+public final class ExpiryStrategy extends Object {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructor
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>ExpiryStrategy</code>.
+    *
+    * @param timeOut
+    *    the time-out, in milliseconds.
+    *
+    * @param precision
+    *    the time-out precision, in milliseconds.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>timeOut &lt; 1
+    *          || precision &lt; 1
+    *          || timeOut &lt; precision</code>
+    */
+   private ExpiryStrategy(long timeOut, long precision) {
+      _timeOut   = timeOut;
+      _precision = precision;
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   /**
+    * The time-out, in milliseconds.
+    */
+   public final long _timeOut;
+
+   /**
+    * The time-out precision, in milliseconds.
+    */
+   public final long _precision;
+
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+}
