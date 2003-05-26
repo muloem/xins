2e8f3aad867c2fb1cbe065a77dddf548c17ae18a
znerd <>
2003-05-26 13:14:24 +0000

    Initial version.

---
+++ src/java-common/org/xins/util/collections/CollectionUtils.java
@@ -0,0 +1,47 @@
+/*
+ * $Id$
+ */
+package org.xins.util.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Utility functions for collections.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:ernst.dehaan@nl.wanadoo.com">ernst.dehaan@nl.wanadoo.com</a>)
+ */
+public final class CollectionUtils extends Object {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   /**
+    * An unmodifiable empty map.
+    */
+   public final static Map EMPTY_MAP = Collections.unmodifiableMap(new HashMap());
+
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructor
+   //-------------------------------------------------------------------------
+
+   /**
+    * Private constructor, no instances of this class should ever be
+    * constructed.
+    */
+   private CollectionUtils() {
+      // empty
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+}
