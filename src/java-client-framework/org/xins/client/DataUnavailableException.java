1d27fe6704069ae4cc76f73c2b371cef43eda721
znerd <>
2003-06-02 09:25:17 +0000

    Initial version.

---
+++ src/java-client-framework/org/xins/client/DataUnavailableException.java
@@ -0,0 +1,44 @@
+/*
+ * $Id$
+ */
+package org.xins.client;
+
+import org.xins.util.MandatoryArgumentChecker;
+
+/**
+ * Exception that indicates that requested data is not available.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
+ */
+public final class DataUnavailableException
+extends Exception {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>DataUnavailableException</code>.
+    */
+   public DataUnavailableException() {
+      // empty
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+}
---
+++ src/java-client-framework/org/xins/client/UnsuccessfulCallException.java
@@ -0,0 +1,72 @@
+/*
+ * $Id$
+ */
+package org.xins.client;
+
+import org.xins.util.MandatoryArgumentChecker;
+
+/**
+ * Exception that indicates that an API call result was unsuccessful.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
+ */
+public final class UnsuccessfulCallException
+extends Exception {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>UnsuccessfulCallException</code> with the
+    * specified call result.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>result == null || result.isSuccess()</code>.
+    */
+   public UnsuccessfulCallException(CallResult result)
+   throws IllegalArgumentException {
+
+      // Check preconditions
+      MandatoryArgumentChecker.check("result", result);
+      if (result.isSuccess()) {
+         throw new IllegalArgumentException("result.isSuccess() == true");
+      }
+
+      _result = result;
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   /**
+    * The call result. The value of this field cannot be <code>null</code>.
+    */
+   private final CallResult _result;
+
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+
+   /**
+    * Returns the call result.
+    *
+    * @return
+    *    the call result, cannot be <code>null</code>.
+    */
+   public CallResult getCallResult() {
+      return _result;
+   }
+}
