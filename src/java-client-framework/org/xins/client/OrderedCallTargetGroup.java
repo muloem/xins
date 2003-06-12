2f72fd530871c3c4f1f4965da61b72a762ff288e
znerd <>
2003-06-12 10:01:19 +0000

    Initial version.

---
+++ src/java-client-framework/org/xins/client/OrderedCallTargetGroup.java
@@ -0,0 +1,60 @@
+/*
+ * $Id$
+ */
+package org.xins.client;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import org.xins.util.MandatoryArgumentChecker;
+
+/**
+ * Call target group that tries to call the underlying function callers in
+ * order.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
+ */
+final class OrderedCallTargetGroup extends CallTargetGroup {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>OrderedCallTargetGroup</code>.
+    *
+    * @param members
+    *    the members of this group, cannot be <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>members == null</code>.
+    */
+   OrderedCallTargetGroup(List members) throws IllegalArgumentException {
+      super(ORDERED_TYPE, members);
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+
+   CallResult callImpl(String sessionID,
+                                String functionName,
+                                Map    parameters)
+   throws IllegalArgumentException, IOException, InvalidCallResultException {
+      return null; // TODO
+   }
+}
