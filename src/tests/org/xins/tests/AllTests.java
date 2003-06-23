1fcbb2101791dfff43412e8875762e381ce65646
znerd <>
2003-06-23 09:12:08 +0000

    Added BasicResponseValidatorTests.

--- src/tests/org/xins/tests/AllTests.java
+++ src/tests/org/xins/tests/AllTests.java
@@ -27,6 +27,7 @@ public class AllTests extends TestSuite {
    public static Test suite() {
       TestSuite suite = new TestSuite();
       suite.addTestSuite(org.xins.tests.client.FunctionCallerParserTests.class);
+      suite.addTestSuite(org.xins.tests.server.BasicResponseValidatorTests.class);
       return suite;
    }
 
