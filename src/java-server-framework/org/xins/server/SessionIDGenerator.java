bc191c0d8d1ce8afe547796d6436120dca153bc4
znerd <>
2003-07-01 13:52:38 +0000

    Simplified.

--- src/java-server-framework/org/xins/server/SessionIDGenerator.java
+++ src/java-server-framework/org/xins/server/SessionIDGenerator.java
@@ -30,19 +30,10 @@ extends Object {
    //-------------------------------------------------------------------------
 
    /**
-    * Constructs a new <code>SessionIDGenerator</code> for the specified API.
-    *
-    * @param api
-    *    the api for which to create this session ID generator, cannot be
-    *    <code>null</code>.
-    *
-    * @throws IllegalArgumentException
-    *    if <code>api == null</code>.
+    * Constructs a new <code>SessionIDGenerator</code>.
     */
-   protected SessionIDGenerator(API api) {
-      MandatoryArgumentChecker.check("api", api);
-      _api  = api;
-      _type = api.getSessionIDType();
+   protected SessionIDGenerator() {
+      // empty
    }
 
 
@@ -50,51 +41,15 @@ extends Object {
    // Fields
    //-------------------------------------------------------------------------
 
-   /**
-    * The API for which this session ID generator generates session IDs.
-    */
-   private final API _api;
-
-   /**
-    * The session ID type.
-    */
-   private final Type _type;
-
-
    //-------------------------------------------------------------------------
    // Methods
    //-------------------------------------------------------------------------
 
    /**
-    * Generates a session ID (wrapper method).
+    * Generates a session ID.
     *
     * @return
-    *    the generated session ID, cannot be <code>null</code>.
-    *
-    * @throws InternalError
-    *    if {@link #generateSessionIDImpl()} returned <code>null</code> or if
-    *    it returns a session ID that is not a valid value for the API session
-    *    ID type.
+    *    the generated session ID, not be <code>null</code>.
     */
-   public final String generateSessionID() throws InternalError {
-      String id = generateSessionIDImpl();
-      if (id == null) {
-         throw new InternalError(getClass().getName() + ".generateSessionIDImpl() returned null.");
-      }
-
-      if (_type.isValidValue(id) == false) {
-         throw new InternalError(getClass().getName() + ".generateSessionIDImpl() returned a value (\"" + id + "\") that is invalid for the session ID type for the API.");
-      }
-
-      return id;
-   }
-
-   /**
-    * Generates a session ID (actual implementation).
-    *
-    * @return
-    *    the generated session ID, should not be <code>null</code> and should
-    *    conform to the API session ID type.
-    */
-   protected abstract String generateSessionIDImpl();
+   public abstract String generateSessionID();
 }
