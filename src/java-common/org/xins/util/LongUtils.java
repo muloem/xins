a6d777683d29f0b7e5e9c190eb56f0e9b714753c
znerd <>
2003-07-01 10:54:55 +0000

    Added parseHexString(String).

--- src/java-common/org/xins/util/LongUtils.java
+++ src/java-common/org/xins/util/LongUtils.java
@@ -40,7 +40,8 @@ public class LongUtils extends Object {
     *    the number to be converted to a text string.
     *
     * @return
-    *    the text string, cannot be <code>null</code>.
+    *    the text string, cannot be <code>null</code>, the length is always 16
+    *    (i.e. <code><em>return</em>.</code>{@link String#length() length()}<code> == 16</code>).
     */
    public static String toHexString(long n) {
 
@@ -62,6 +63,52 @@ public class LongUtils extends Object {
       }
 
       return new String(chars, 0, bufSize);
+   }
+
+   /**
+    * Parses the specified 16-digit hex string.
+    *
+    * @param s
+    *    the hexadecimal string, cannot be <code>null</code> and must have
+    *    size 16
+    *    (i.e. <code>s.</code>{@link String#length() length()}<code> == 16</code>).
+    *
+    * @throws IllegalArgumentException
+    *    if <code>s == null || s.</code>{@link String#length() length()}<code> != 16</code>.
+    *
+    * @throws NumberFormatException
+    *    if any of the characters in the specified string is not a hex digit
+    *    (<code>'0'</code> to <code>'9'</code> and <code>'a'</code> to
+    *    <code>'f'</code>).
+    */
+   public static long parseHexString(String s)
+   throws IllegalArgumentException, NumberFormatException {
+
+      // Check preconditions
+      if (s == null) {
+         throw new IllegalArgumentException("s == null");
+      } else if (s.length() != 16) {
+         throw new IllegalArgumentException("s.length() != 16");
+      }
+
+      long n = 0L;
+
+      // Loop through all characters
+      for (int i = 0; i < 16; i++) {
+         char c = s.charAt(i);
+         n <<= 4;
+         if (c >= '0' && c <= '9') {
+            final int CHAR_ZERO = (int) '0';
+            n &= ((int) c) - CHAR_ZERO;
+         } else if (c >= 'a' && c <= 'f') {
+            final int CHAR_A = (int) 'a';
+            n &= ((int) c) - CHAR_A;
+         } else {
+            throw new NumberFormatException("s.charAt(" + i + ") == '" + c + '\''); 
+         }
+      }
+
+      return n;
    }
 
 
