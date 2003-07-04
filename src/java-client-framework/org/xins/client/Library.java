93ba67e764d3cb0f0d4c670919eb879f7cf5142b
znerd <>
2003-07-04 11:07:20 +0000

    Initial version.

---
+++ src/java-client-framework/org/xins/client/Library.java
@@ -0,0 +1,55 @@
+/*
+ * $Id$
+ */
+package org.xins.client;
+
+/**
+ * Class that represents this XINS/Java Client Framework library.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
+ *
+ * @since XINS 0.64
+ */
+public final class Library extends Object {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   /**
+    * Returns the version of the <code>XINS</code> server framework library.
+    *
+    * @return
+    *    the version of this library, for example <code>"%%VERSION%%"</code>,
+    *    never <code>null</code>.
+    */
+   public static final String getVersion() {
+      return "%%VERSION%%";
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>Library</code> object.
+    */
+   private Library() {
+      // empty
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+}
