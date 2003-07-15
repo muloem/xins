8dd08691376c0a0c75c06bcadb3b9bc360bf945e
znerd <>
2003-07-15 12:45:36 +0000

    Initial version.

---
+++ src/java-common/org/xins/util/BooleanConstants.java
@@ -0,0 +1,54 @@
+/*
+ * $Id$
+ */
+package org.xins.util;
+
+/**
+ * Constant instances for class <code>java.util.Boolean</code>.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
+ *
+ * @since XINS 0.74
+ */
+public final class BooleanConstants extends Object {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constant <code>Boolean</code> for the value <code>true</code>.
+    */
+   public static final Boolean TRUE = new Boolean(true);
+
+   /**
+    * Constant <code>Boolean</code> for the value <code>false</code>.
+    */
+   public static final Boolean FALSE = new Boolean(false);
+
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>BooleanConstants</code> object.
+    */
+   private BooleanConstants() {
+      // empty
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+}
