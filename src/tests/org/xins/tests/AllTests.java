7b1912e0a446dddf5d8d94c425e604487cf6e12f
znerd <>
2003-07-22 12:49:25 +0000

    Added org.xins.tests.common.util.text.ReplacerTests.

--- src/tests/org/xins/tests/AllTests.java
+++ src/tests/org/xins/tests/AllTests.java
@@ -28,6 +28,7 @@ public class AllTests extends TestSuite {
       TestSuite suite = new TestSuite();
       suite.addTestSuite(org.xins.tests.client.FunctionCallerParserTests.class);
       suite.addTestSuite(org.xins.tests.common.util.text.HexConverterTests.class);
+      suite.addTestSuite(org.xins.tests.common.util.text.ReplacerTests.class);
       suite.addTestSuite(org.xins.tests.server.BasicResponseValidatorTests.class);
       return suite;
    }
