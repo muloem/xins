1604d339421ccd506e96fde687fb66e014dbe024
znerd <>
2003-09-02 11:02:10 +0000

    Added doCall() and doCallImpl() methods.

--- src/java-common/org/xins/util/service/Service.java
+++ src/java-common/org/xins/util/service/Service.java
@@ -3,6 +3,8 @@
  */
 package org.xins.util.service;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import org.xins.util.MandatoryArgumentChecker;
 
 /**
@@ -71,4 +73,83 @@ public abstract class Service extends Object {
    public Descriptor getDescriptor() {
       return _descriptor;
    }
+
+   /**
+    * Performs a call using the specified subject. Target
+    * {@link ServiceDescriptor service descriptors} will be picked and passed
+    * to {@link #doCallImpl(ServiceDescriptor,Object)} until there is one that
+    * succeeds. If one of the calls succeeds, then the result is returned. If
+    * none succeeds, then a {@link CallFailedException} is thrown.
+    *
+    * @param subject
+    *    the subject passed, could possibly be <code>null</code>.
+    *
+    * @return
+    *    a combination of the call result and a link to the
+    *    {@link ServiceDescriptor target} that returned this result, if and
+    *    only if one of the calls succeeded, could be <code>null</code>.
+    *
+    * @throws CallFailedException
+    *    if all calls failed.
+    */
+   protected final CallResult doCall(Object subject)
+   throws CallFailedException {
+
+      ArrayList failedTargets = null;
+      ArrayList exceptions    = null;
+
+      // Iterate over all targets
+      Iterator iterator = _descriptor.iterateServices();
+      while (iterator.hasNext()) {
+
+         // Determine the service descriptor target
+         ServiceDescriptor target = (ServiceDescriptor) iterator.next();
+
+         // Call using this target
+         try {
+            Object result = doCallImpl(target, subject);
+            if (failedTargets != null) {
+               failedTargets.trimToSize();
+               exceptions.trimToSize();
+            }
+            return new CallResult(failedTargets, exceptions, target, result);
+
+         // If it fails, store the exception and try the next
+         } catch (Throwable exception) {
+            if (failedTargets == null) {
+               failedTargets = new ArrayList();
+               exceptions    = new ArrayList();
+            }
+            failedTargets.add(target);
+            exceptions.add(exception);
+         }
+      }
+
+      // Loop ended, all calls failed
+      throw new CallFailedException(subject, failedTargets, exceptions);
+   }
+
+   /**
+    * Calls the specified target using the specified subject. This method must
+    * be implemented by subclasses. It is called as soon as a target is
+    * selected to be called. If the call fails, then an exception should be
+    * thrown. If the call succeeds, then the call result should be returned
+    * from this method.
+    *
+    * @param target
+    *    the target to call, cannot be <code>null</code>.
+    *
+    * @param subject
+    *    the subject passed, could possibly be <code>null</code>.
+    *
+    * @return
+    *    the result, if and only if the call succeeded, could be
+    *    <code>null</code>.
+    *
+    * @throws Throwable
+    *    if the call to the specified target failed.
+    */
+   protected abstract Object doCallImpl(ServiceDescriptor target,
+                                        Object            subject)
+   throws Throwable;
 }
