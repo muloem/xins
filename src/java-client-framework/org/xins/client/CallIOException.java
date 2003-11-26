234e97d38c8730141dba769b776837c6a2595ce2
znerd <>
2003-11-26 06:27:57 +0000

    Initial version.

---
+++ src/java-client-framework/org/xins/client/CallIOException.java
@@ -0,0 +1,55 @@
+/*
+ * $Id$
+ */
+package org.xins.client;
+
+import java.io.IOException;
+import org.xins.util.MandatoryArgumentChecker;
+
+/**
+ * Exception thrown to indicate that a XINS API call failed due to an I/O
+ * error.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
+ */
+public final class CallIOException
+extends CallException {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>CallIOException</code> based on an
+    * <code>IOException</code>.
+    *
+    * @param ioException
+    *    the cause {@link IOException}, cannot be <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>ioException == null</code>.
+    */
+   public CallIOException(IOException ioException)
+   throws IllegalArgumentException {
+      super(ioException.getMessage(), ioException);
+      MandatoryArgumentChecker.check("ioException", ioException);
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+}
