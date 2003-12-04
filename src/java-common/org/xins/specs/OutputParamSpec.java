197d3f9a26976ddf0d0c6e22f094d902e6fca6a1
znerd <>
2003-12-04 09:59:57 +0000

    Initial version.

---
+++ src/java-common/org/xins/specs/OutputParamSpec.java
@@ -0,0 +1,94 @@
+/*
+ * $Id$
+ */
+package org.xins.specs;
+
+import org.xins.util.MandatoryArgumentChecker;
+
+/**
+ * Specification for an output parameter for a function.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
+ */
+public class OutputParamSpec
+extends Spec {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   /**
+    * The type for output parameter components.
+    */
+   private static final Type TYPE = new Type();
+
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>OutputParamSpec</code> for an output parameter
+    * with the specified name and version.
+    *
+    * @param name
+    *    the name for the component, not <code>null</code>.
+    *
+    * @param version
+    *    the version for the component, not <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>name == null || version == null</code>.
+    */
+   public OutputParamSpec(String name, String version)
+   throws IllegalArgumentException {
+      super(TYPE, name, version);
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Inner classes
+   //-------------------------------------------------------------------------
+
+   /**
+    * The type for an output parameter component.
+    *
+    * @version $Revision$ $Date$
+    * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
+    */
+   private static class Type extends SpecType {
+
+      //----------------------------------------------------------------------
+      // Constructors
+      //----------------------------------------------------------------------
+
+      /**
+       * Constructs a new <code>Type</code> object.
+       */
+      private Type() {
+         super("function output parameter");
+      }
+
+
+      //----------------------------------------------------------------------
+      // Fields
+      //----------------------------------------------------------------------
+
+      //----------------------------------------------------------------------
+      // Methods
+      //----------------------------------------------------------------------
+   }
+}
