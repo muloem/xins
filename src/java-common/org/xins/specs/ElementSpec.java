187d2db32daa5f8e7d7a437ca5ca23efa084d66c
znerd <>
2003-12-04 10:31:51 +0000

    Initial version.

---
+++ src/java-common/org/xins/specs/ElementSpec.java
@@ -0,0 +1,94 @@
+/*
+ * $Id$
+ */
+package org.xins.specs;
+
+import org.xins.util.MandatoryArgumentChecker;
+
+/**
+ * Specification for an element in a function data section.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
+ */
+public class ElementSpec
+extends Spec {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   /**
+    * The type for element components.
+    */
+   private static final Type TYPE = new Type();
+
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>ElementSpec</code> for an element with the
+    * specified name and version.
+    *
+    * @param name
+    *    the name for the component, not <code>null</code>.
+    *
+    * @param version
+    *    the version for the component, not <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>name == null || version == null</code>.
+    */
+   public ElementSpec(String name, String version)
+   throws IllegalArgumentException {
+      super(TYPE, name, version);
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Inner classes
+   //-------------------------------------------------------------------------
+
+   /**
+    * The type for an element component.
+    *
+    * @version $Revision$ $Date$
+    * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
+    */
+   private static class Type extends SpecType {
+
+      //----------------------------------------------------------------------
+      // Constructors
+      //----------------------------------------------------------------------
+
+      /**
+       * Constructs a new <code>Type</code> object.
+       */
+      private Type() {
+         super("element");
+      }
+
+
+      //----------------------------------------------------------------------
+      // Fields
+      //----------------------------------------------------------------------
+
+      //----------------------------------------------------------------------
+      // Methods
+      //----------------------------------------------------------------------
+   }
+}
