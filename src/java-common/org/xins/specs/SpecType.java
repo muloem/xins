43891eb1b2f1553bdcff778796dbcef70b3f5156
znerd <>
2003-12-04 09:29:38 +0000

    Initial version.

---
+++ src/java-common/org/xins/specs/SpecType.java
@@ -0,0 +1,72 @@
+/*
+ * $Id$
+ */
+package org.xins.specs;
+
+import org.xins.util.MandatoryArgumentChecker;
+
+/**
+ * Type of a component of a XINS API specification.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
+ *
+ * @see Spec
+ */
+public abstract class SpecType
+extends Object {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>SpecType</code> with the specified name.
+    *
+    * @param name
+    *    the name for the type, not <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>name == null</code>.
+    */
+   SpecType(String name) throws IllegalArgumentException {
+
+      // Check preconditions
+      MandatoryArgumentChecker.check("name", name);
+
+      _name    = name;
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   /**
+    * The name of the type. Cannot be <code>null</code>.
+    */
+   private final String _name;
+
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+
+   /**
+    * Returns the name of the component.
+    *
+    * @return
+    *    the name, cannot be <code>null</code>.
+    */
+   public final String getName() {
+      return _name;
+   }
+}
