2ff2a20609358f9c7682ddb7c89a9234f6d24172
znerd <>
2003-12-22 07:07:05 +0000

    Initial version.

---
+++ src/java-common/org/xins/util/math/SafeRandom.java
@@ -0,0 +1,76 @@
+/*
+ * $Id$
+ */
+package org.xins.util;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+/**
+ * Random generator facade that uses the securest available random generator.
+ * The implementation creates a {@link SecureRandom} and a {@link Random}
+ * object. A request for a random number will first attempt to use the
+ * {@link SecureRandom} object. If that fails, then the {@link Random} object
+ * will be used.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
+ */
+public final class SafeRandom extends Object {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>SafeRandom</code> object.
+    */
+   public SafeRandom() {
+      _secureRandom = new SecureRandom();
+      _random       = new Random();
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   /**
+    * The <code>SecureRandom</code> object, the preferred random generator to
+    * use.
+    */
+   private SecureRandom _secureRandom;
+
+   /**
+    * The <code>Random</code> object, the least preferred random generator to
+    * use.
+    */
+   private Random _random;
+
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+
+   /**
+    * Generates the next (pseudo)random <code>int</code> value.
+    *
+    * @return
+    *    the next (pseudo)random number, can be any <code>int</code> value.
+    */
+   public int nextInt() {
+      try {
+         return _secureRandom.nextInt();
+      } catch (Throwable exception) {
+         return _random.nextInt();
+      }
+   }
+}
