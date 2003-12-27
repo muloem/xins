5a0377ae95988e07b6d20a26a2872eec2d5326eb
znerd <>
2003-12-27 15:17:48 +0000

    Added name and logging support.

--- src/java-common/org/xins/util/math/SafeRandom.java
+++ src/java-common/org/xins/util/math/SafeRandom.java
@@ -5,6 +5,9 @@ package org.xins.util.math;
 
 import java.security.SecureRandom;
 import java.util.Random;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.xins.util.MandatoryArgumentChecker;
 
 /**
  * Random generator facade that uses the securest available random generator.
@@ -22,6 +25,12 @@ public final class SafeRandom extends Object {
    // Class fields
    //-------------------------------------------------------------------------
 
+   /**
+    * Logger for this class.
+    */
+   private static final Logger LOG = Logger.getLogger(SafeRandom.class.getName());
+
+
    //-------------------------------------------------------------------------
    // Class functions
    //-------------------------------------------------------------------------
@@ -31,9 +40,23 @@ public final class SafeRandom extends Object {
    //-------------------------------------------------------------------------
 
    /**
-    * Constructs a new <code>SafeRandom</code> object.
+    * Constructs a new <code>SafeRandom</code> object with the specified name.
+    * The name is used in log messages.
+    *
+    * @param name
+    *    the name, cannot be <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>name == null</code>.
     */
-   public SafeRandom() {
+   public SafeRandom(String name)
+   throws IllegalArgumentException {
+
+      // Check preconditions
+      MandatoryArgumentChecker.check("name", name);
+
+      // Initialize instance fields
+      _name         = name;
       _secureRandom = new SecureRandom();
       _random       = new Random();
    }
@@ -44,16 +67,21 @@ public final class SafeRandom extends Object {
    //-------------------------------------------------------------------------
 
    /**
-    * The <code>SecureRandom</code> object, the preferred random generator to
-    * use.
+    * The name. Cannot be <code>null</code>.
     */
-   private SecureRandom _secureRandom;
+   private final String _name;
+
+   /**
+    * The <code>SecureRandom</code> object, the preferred random generator to
+    * use. Cannot be <code>null</code>.
+    */
+   private final SecureRandom _secureRandom;
 
    /**
     * The <code>Random</code> object, the least preferred random generator to
-    * use.
+    * use. Cannot be <code>null</code>.
     */
-   private Random _random;
+   private final Random _random;
 
 
    //-------------------------------------------------------------------------
@@ -67,9 +95,13 @@ public final class SafeRandom extends Object {
     *    the next (pseudo)random number, can be any <code>int</code> value.
     */
    public int nextInt() {
+
       try {
          return _secureRandom.nextInt();
       } catch (Throwable exception) {
+         if (LOG.isEnabledFor(Level.WARN)) {
+            LOG.warn("SafeRandom \"" + _name + "\": Failed to generate secure random int, falling back to pseudo-random.");
+         }
          return _random.nextInt();
       }
    }
@@ -84,6 +116,9 @@ public final class SafeRandom extends Object {
       try {
          return _secureRandom.nextLong();
       } catch (Throwable exception) {
+         if (LOG.isEnabledFor(Level.WARN)) {
+            LOG.warn("SafeRandom \"" + _name + "\": Failed to generate secure random long, falling back to pseudo-random.");
+         }
          return _random.nextLong();
       }
    }
