796b3ec66e79ceaf05656f6cb78b5419610de15f
znerd <>
2003-12-31 14:09:10 +0000

    Added call() methods.

--- src/java-client-framework/org/xins/client/XINSServiceCaller.java
+++ src/java-client-framework/org/xins/client/XINSServiceCaller.java
@@ -181,14 +181,141 @@ public final class XINSServiceCaller extends ServiceCaller {
    // Methods
    //-------------------------------------------------------------------------
 
-   // TODO: call(...) method
-
    protected Object doCallImpl(TargetDescriptor target,
                                Object           subject)
    throws Throwable {
 
       // TODO
       return null;
+   }
+
+   /**
+    * Calls the specified session-less API function, with no parameters.
+    *
+    * @param functionName
+    *    the name of the function to be called, not <code>null</code>.
+    *
+    * @return
+    *    the call result, never <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>functionName == null</code>.
+    *
+    * @throws CallIOException
+    *    if the API could not be contacted due to an I/O error.
+    *
+    * @throws InvalidCallResultException
+    *    if the calling of the function failed or if the result from the
+    *    function was invalid.
+    */
+   public XINSServiceCaller.Result call(String functionName)
+   throws IllegalArgumentException,
+          CallIOException,
+          InvalidCallResultException {
+      return call(null, functionName, null);
+   }
+
+   /**
+    * Calls the specified session-less API function with the specified
+    * parameters.
+    *
+    * @param functionName
+    *    the name of the function to be called, not <code>null</code>.
+    *
+    * @param parameters
+    *    the parameters to be passed to that function, or
+    *    <code>null</code>; keys must be {@link String Strings}, values can be
+    *    of any class.
+    *
+    * @return
+    *    the call result, never <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>functionName == null</code>.
+    *
+    * @throws CallIOException
+    *    if the API could not be contacted due to an I/O error.
+    *
+    * @throws InvalidCallResultException
+    *    if the calling of the function failed or if the result from the
+    *    function was invalid.
+    */
+   public XINSServiceCaller.Result call(String functionName,
+                                        Map    parameters)
+   throws IllegalArgumentException,
+          CallIOException,
+          InvalidCallResultException {
+      return call(null, functionName, parameters);
+   }
+
+   /**
+    * Calls the specified API function with the specified parameters.
+    *
+    * @param sessionID
+    *    the session identifier, if any, or <code>null</code> if the function
+    *    is session-less.
+    *
+    * @param functionName
+    *    the name of the function to be called, not <code>null</code>.
+    *
+    * @param parameters
+    *    the parameters to be passed to that function, or
+    *    <code>null</code>; keys must be {@link String Strings}, values can be
+    *    of any class.
+    *
+    * @return
+    *    the call result, never <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>functionName == null</code>.
+    *
+    * @throws CallIOException
+    *    if the API could not be contacted due to an I/O error.
+    *
+    * @throws InvalidCallResultException
+    *    if the calling of the function failed or if the result from the
+    *    function was invalid.
+    */
+   public XINSServiceCaller.Result call(String sessionID,
+                                        String functionName,
+                                        Map    parameters)
+   throws IllegalArgumentException,
+          CallIOException,
+          InvalidCallResultException {
+      return null; // TODO
+   }
+
+   /**
+    * Executes the specified request.
+    *
+    * @param request
+    *    the request to execute, cannot be <code>null</code>.
+    *
+    * @return
+    *    the call result, never <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>request == null</code>.
+    *
+    * @throws CallIOException
+    *    if the API could not be contacted due to an I/O error.
+    *
+    * @throws InvalidCallResultException
+    *    if the calling of the function failed or if the result from the
+    *    function was invalid.
+    */
+   public XINSServiceCaller.Result call(CallRequest request)
+   throws IllegalArgumentException,
+          CallIOException,
+          InvalidCallResultException {
+
+      // Check preconditions
+      MandatoryArgumentChecker.check("request", request);
+
+      // Forward the request to the primary call() method
+      return call(request.getSessionID(),
+                  request.getFunctionName(),
+                  request.getParameters());
    }
 
 
