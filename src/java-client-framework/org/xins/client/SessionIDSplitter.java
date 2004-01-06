cc8623f5cc6d517320a38c305e67d33666827f8c
znerd <>
2004-01-06 08:10:39 +0000

    Fixed @link.

--- src/java-client-framework/org/xins/client/SessionIDSplitter.java
+++ src/java-client-framework/org/xins/client/SessionIDSplitter.java
@@ -26,7 +26,7 @@ public interface SessionIDSplitter {
     * <ul>
     *    <li>the first element in the array should be set to the CRC-32 of the
     *        target API URL, as a {@link String} (see
-    *        {@link TargetDescriptor#getCRC32()}).
+    *        {@link TargetDescriptor#getCRC()}).
     *    <li>the second element in the array should be set to the server-side
     *        session identifier, specific to the target API (which is
     *        identified by the the CRC-32 checksum in the first element.)
