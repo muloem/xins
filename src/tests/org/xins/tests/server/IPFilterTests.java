3de7e2f12839d50f1cb301a02a32c9785677c3ea
znerd <>
2004-01-27 13:59:25 +0000

    Fixed last commit.

--- src/tests/org/xins/tests/server/IPFilterTests.java
+++ src/tests/org/xins/tests/server/IPFilterTests.java
@@ -184,7 +184,7 @@ public class IPFilterTests extends TestCase {
       doTestIsAuthorized(filter, "194.134.168.212",          true,  false);
       doTestIsAuthorized(filter, "194.134.168.214",          true,  false);
 
-      IPFilter filter = IPFilter.parseFilter("194.134.168.213/31");
+      filter = IPFilter.parseFilter("194.134.168.213/31");
       assertNotNull(filter);
 
       doTestIsAuthorized(filter, "194.134.168.213",          true,  true);
