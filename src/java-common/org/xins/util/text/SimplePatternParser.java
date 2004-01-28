fbd92317fe5d3bc5cdcefbff979d6e9ba8399dd1
pajtroon <>
2004-01-28 14:49:15 +0000

    Added initial version of mechanism to parse the simple pattern.

--- src/java-common/org/xins/util/text/SimplePatternParser.java
+++ src/java-common/org/xins/util/text/SimplePatternParser.java
@@ -3,6 +3,9 @@
  */
 package org.xins.util.text;
 
+import org.apache.oro.text.regex.MalformedPatternException;
+import org.apache.oro.text.regex.Pattern;
+import org.apache.oro.text.regex.Perl5Compiler;
 import org.apache.oro.text.regex.Perl5Pattern;
 import org.xins.util.MandatoryArgumentChecker;
 
@@ -35,6 +38,7 @@ import org.xins.util.MandatoryArgumentChecker;
  *
  * @version $Revision$ $Date$
  * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
+ * @author Peter Troon (<a href="mailto:peter.troon@nl.wanadoo.com">peter.troon@nl.wanadoo.com</a>)
  *
  * @since XINS 0.152
  */
@@ -79,6 +83,65 @@ public class SimplePatternParser extends Object {
     */
    public Perl5Pattern parseSimplePattern(String simplePattern)
    throws IllegalArgumentException, ParseException {
-      return null; // TODO
+
+      MandatoryArgumentChecker.check("simplePattern", simplePattern);
+
+      if (isValidPattern(simplePattern) == false) {
+         throw new ParseException("The pattern '" + simplePattern + "' is invalid.");
+      }
+
+      simplePattern = createPerl5RegularExpression(simplePattern);
+
+      Perl5Pattern perl5pattern = null;
+      Perl5Compiler perl5compiler = new Perl5Compiler();
+
+      boolean parseError = false;
+      try {
+         Pattern pattern = perl5compiler.compile(simplePattern);
+         if (pattern instanceof Perl5Pattern) {
+            perl5pattern = (Perl5Pattern) pattern;
+         } else {
+            parseError = true;
+         }
+      } catch (
+         MalformedPatternException mpe) {
+         parseError = true;
+      }
+
+      if (parseError == true) {
+         throw new ParseException("An error occurred while parsing the pattern '" + simplePattern + "'.");
+      }
+
+      return perl5pattern;
+   }
+
+
+   private String createPerl5RegularExpression(String pattern) {
+      pattern = pattern.replaceAll("\\*", ".*");
+      pattern = pattern.replace('?', '.');
+      pattern = '^' + pattern + '$';
+      return pattern;
+   }
+
+
+
+   private boolean isValidPattern(String pattern) {
+      char[] patternContents = pattern.toCharArray();
+      int patternContentsLength = patternContents.length;
+      boolean valid = true;
+
+      for (int i= 0;i < patternContentsLength && valid == true; i++) {
+         char currChar = patternContents[i];
+         if (i < patternContentsLength - 1) {
+            if (currChar == '*' || currChar == '?' || currChar == '^' || currChar == '$') {
+               char nextChar = patternContents[i + 1];
+               if (nextChar == '*' || nextChar == '?' || nextChar == '^' || nextChar == '$') {
+                  valid = false;
+               }
+            }
+         }
+      }
+
+      return valid;
    }
 }
