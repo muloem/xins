68c1b5464e2e97449b3984b6e40393589b5de027
znerd <>
2004-02-03 09:57:55 +0000

    Extended set of tests.

--- src/tests/org/xins/tests/server/AccessRuleTests.java
+++ src/tests/org/xins/tests/server/AccessRuleTests.java
@@ -7,6 +7,8 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import org.xins.server.AccessRule;
+import org.xins.server.IPFilter;
+import org.xins.util.text.FastStringBuffer;
 import org.xins.util.text.ParseException;
 
 /**
@@ -75,5 +77,52 @@ public class AccessRuleTests extends TestCase {
       } catch (IllegalArgumentException exception) {
          // as expected
       }
+
+      doTestParseAccessRule("1.2.3.4");
+   }
+
+   private void doTestParseAccessRule(String ip)
+   throws Throwable {
+      for (int mask = 0; mask <= 32; mask++) {
+
+         doTestParseAccessRule(false, ip, mask);
+         doTestParseAccessRule(true, ip, mask);
+      }
+   }
+
+   private void doTestParseAccessRule(boolean allow, String ip, int mask)
+   throws Throwable {
+      doTestParseAccessRule(allow, ip, mask, " ",        " ");
+      doTestParseAccessRule(allow, ip, mask, "\t",       "\t");
+      doTestParseAccessRule(allow, ip, mask, " ",        "\t");
+      doTestParseAccessRule(allow, ip, mask, " \t\n\r ", "\t\n\r");
+      doTestParseAccessRule(allow, ip, mask, "\n ",      "\r");
+   }
+
+   private void doTestParseAccessRule(boolean allow, String ip, int mask, String whitespace1, String whitespace2)
+   throws Throwable {
+
+      FastStringBuffer buffer = new FastStringBuffer(250);
+      if (allow) {
+         buffer.append("allow");
+      } else {
+         buffer.append("deny");
+      }
+      buffer.append(whitespace1);
+      buffer.append(ip);
+      buffer.append('/');
+      buffer.append(mask);
+      buffer.append(whitespace2);
+      buffer.append("*");
+
+      AccessRule rule = AccessRule.parseAccessRule(buffer.toString());
+      assertNotNull(rule);
+      assertEquals(allow, rule.isAllowRule());
+
+      IPFilter ipFilter = rule.getIPFilter();
+      assertNotNull(ipFilter);
+
+      assertEquals(ip,   ipFilter.getBaseIP());
+      assertEquals(mask, ipFilter.getMask());
    }
 }
