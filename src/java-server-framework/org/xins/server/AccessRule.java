498240e7b5507673acaa0865fae2ee90da3707c1
znerd <>
2004-02-03 07:25:01 +0000

    Added parseAccessRule(String) and constructor with args.

--- src/java-server-framework/org/xins/server/AccessRule.java
+++ src/java-server-framework/org/xins/server/AccessRule.java
@@ -3,7 +3,9 @@
  */
 package org.xins.server;
 
+import org.apache.oro.text.regex.Perl5Pattern;
 import org.xins.util.MandatoryArgumentChecker;
+import org.xins.util.text.ParseException;
 
 /**
  * Access rule.
@@ -22,15 +24,62 @@ extends Object {
    // Class functions
    //-------------------------------------------------------------------------
 
+   /**
+    * Parses the specified character string to construct a new
+    * <code>AccessRule</code> object.
+    *
+    * @param descriptor
+    *    the access rule descriptor, the character string to parse, cannot be <code>null</code>.
+    *
+    * @return
+    *    an {@link AccessRule} instance, never <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>descriptor == null</code>.
+    *
+    * @throws ParseException
+    *    if there was a parsing error.
+    */
+   public static final AccessRule parseAccessRule(String descriptor)
+   throws IllegalArgumentException, ParseException {
+
+      // Check preconditions
+      MandatoryArgumentChecker.check("descriptor", descriptor);
+
+      return null; // TODO
+   }
+
+
    //-------------------------------------------------------------------------
    // Constructors
    //-------------------------------------------------------------------------
 
    /**
     * Constructs a new <code>AccessRule</code>.
+    *
+    * @param allow
+    *    flag that indicates if this rule grants access (<code>true</code>) or
+    *    denies access (<code>false</code>).
+    *
+    * @param ipFilter
+    *    filter used for matching (or not) IP addresses, cannot be
+    *    <code>null</code>.
+    *
+    * @param functionNamePattern
+    *    regular expression used for matching (or not) a function name; cannot be <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>filter == null || functionNamePattern == null</code>.
     */
-   private AccessRule() {
-      // empty
+   private AccessRule(boolean      allow,
+                      IPFilter     ipFilter,
+                      Perl5Pattern functionNamePattern) {
+
+      // Check preconditions
+      MandatoryArgumentChecker.check("ipFilter", ipFilter,
+                                     "functionNamePattern", functionNamePattern);
+
+      // TODO
    }
 
 
