26720e23a09ef8e87aab40fb2de8ee50f0f0b5c1
znerd <>
2004-02-06 07:12:33 +0000

    Added INIT_ACL_LOG.

--- src/java-server-framework/org/xins/server/Library.java
+++ src/java-server-framework/org/xins/server/Library.java
@@ -39,6 +39,13 @@ public final class Library extends Object {
 
    /**
     * The logging category used by the XINS/Java Server Framework during
+    * re-initialization for ACL-related messages. This field is not
+    * <code>null</code>.
+    */
+   static final Logger INIT_ACL_LOG = Logger.getLogger("org.xins.server.lifespan.init.acl");
+
+   /**
+    * The logging category used by the XINS/Java Server Framework during
     * shutdown/deinitialization. This field is not <code>null</code>.
     */
    static final Logger SHUTDOWN_LOG = Logger.getLogger("org.xins.server.lifespan.shutdown");
