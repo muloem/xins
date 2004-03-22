091b9045c7ffe60751e11bc4d7ce9823a3e7f4a6
znerd <>
2004-03-22 13:06:48 +0000

    Moved log level constants from generated Log class to AbstractLog class.

--- src/java-common/org/xins/logdoc/AbstractLog.java
+++ src/java-common/org/xins/logdoc/AbstractLog.java
@@ -16,9 +16,61 @@ extends Object {
    // Class fields
    //-------------------------------------------------------------------------
 
+   /**
+    * The <em>debug</em> log level.
+    */
+   private static final Level DEBUG;
+
+   /**
+    * The <em>info</em> log level.
+    */
+   private static final Level INFO;
+
+   /**
+    * The <em>notice</em> log level.
+    */
+   private static final Level NOTICE;
+
+   /**
+    * The <em>warning</em> log level.
+    */
+   private static final Level WARNING;
+
+   /**
+    * The <em>error</em> log level.
+    */
+   private static final Level ERROR;
+
+   /**
+    * The <em>fatal</em> log level.
+    */
+   private static final Level FATAL;
+
+
    //-------------------------------------------------------------------------
    // Class functions
    //-------------------------------------------------------------------------
+
+   /**
+    * Initializes this class.
+    */
+   static {
+
+      // Determine the int value for the NOTICE level
+      int noticeInt = (Level.INFO_INT + Level.WARN_INT) / 2;
+      if (noticeInt <= Level.INFO_INT || noticeInt >= Level.WARN_INT) {
+         throw new Error("Unable to determine int value for NOTICE level between INFO and WARN. Value for INFO level is " + Level.INFO_INT + ". Value for WARN level is " + Level.WARN_INT + '.');
+      }
+
+      // Initialize all the log levels
+      DEBUG   = Level.DEBUG;
+      INFO    = Level.INFO;
+      NOTICE  = new CustomLevel(noticeInt, "NOTICE", 5);
+      WARNING = Level.WARN;
+      ERROR   = Level.ERROR;
+      FATAL   = Level.FATAL;
+   }
+
 
    //-------------------------------------------------------------------------
    // Constructors
--- src/xslt/logdoc/log_to_Log_java.xslt
+++ src/xslt/logdoc/log_to_Log_java.xslt
@@ -65,36 +65,6 @@ public class Log extends AbstractLog {
 		<xsl:text><![CDATA[.Log";
 
    /**
-    * The <em>debug</em> log level.
-    */
-   private static final Level DEBUG;
-
-   /**
-    * The <em>info</em> log level.
-    */
-   private static final Level INFO;
-
-   /**
-    * The <em>notice</em> log level.
-    */
-   private static final Level NOTICE;
-
-   /**
-    * The <em>warning</em> log level.
-    */
-   private static final Level WARNING;
-
-   /**
-    * The <em>error</em> log level.
-    */
-   private static final Level ERROR;
-
-   /**
-    * The <em>fatal</em> log level.
-    */
-   private static final Level FATAL;
-
-   /**
     * Associations from name to translation bundle.
     */
    private static final HashMap TRANSLATION_BUNDLES_BY_NAME;
@@ -117,20 +87,6 @@ public class Log extends AbstractLog {
     * Initializes this class.
     */
    static {
-
-      // Determine the int value for the NOTICE level
-      int noticeInt = (Level.INFO_INT + Level.WARN_INT) / 2;
-      if (noticeInt <= Level.INFO_INT || noticeInt >= Level.WARN_INT) {
-         throw new Error("Unable to determine int value for NOTICE level between INFO and WARN. Value for INFO level is " + Level.INFO_INT + ". Value for WARN level is " + Level.WARN_INT + '.');
-      }
-
-      // Initialize all the log levels
-      DEBUG   = Level.DEBUG;
-      INFO    = Level.INFO;
-      NOTICE  = new CustomLevel(noticeInt, "NOTICE", 5);
-      WARNING = Level.WARN;
-      ERROR   = Level.ERROR;
-      FATAL   = Level.FATAL;
 
       // Reference all translation bundles by name
       TRANSLATION_BUNDLES_BY_NAME = new HashMap();]]></xsl:text>
