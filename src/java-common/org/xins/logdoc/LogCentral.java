247e1e3da7b368cf1be28340757f8902f5a9927e
znerd <>
2004-03-24 07:16:59 +0000

    Initial version.

---
+++ src/java-common/org/xins/logdoc/LogCentral.java
@@ -0,0 +1,46 @@
+/*
+ * $Id$
+ */
+package org.xins.logdoc;
+
+import org.xins.util.MandatoryArgumentChecker;
+
+/**
+ * Central class for <em>logdoc</em> logging.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:znerd@FreeBSD.org">znerd@FreeBSD.org</a>)
+ */
+public final class LogCentral
+extends Object {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>LogCentral</code> instance. This constructor is
+    * intentionally made <code>private</code>, since no instances should be
+    * constructed of this class.
+    */
+   private LogCentral() {
+      // empty
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+}
