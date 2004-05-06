0e267a8ed3443a31336ab452378421cc59f97128
agoubard <>
2004-05-06 15:20:08 +0000

    - now null is accepted as a key

--- src/tests/org/xins/tests/common/types/standard/PropertiesTests.java
+++ src/tests/org/xins/tests/common/types/standard/PropertiesTests.java
@@ -108,7 +108,7 @@ public class PropertiesTests extends TestCase {
          m.put(name, pr.get(name));
          count++;
       }
-      assertEquals(2, count);
+      assertEquals(3, count);
 
       assertEquals(m.get(name1), value1);
       assertEquals(m.get(name2), null);
