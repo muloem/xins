c8e86b9954685b3c1fba56c798f989a8e3cf2d30
meesw <>
2004-07-13 12:05:00 +0000

    initial version.

---
+++ src/tests/org/xins/tests/common/collections/BasicPropertyReaderTests.java
@@ -0,0 +1,75 @@
+/*
+ * $Id$
+ */
+package org.xins.tests.common.collections;
+
+import junit.framework.TestCase;
+
+/**
+ * @TODO Fill in the class description here.
+ *
+ * @version $Revision$
+ * @author Mees Witteman (<a href="mailto:mees.witteman@nl.wanadoo.com">mees.witteman@nl.wanadoo.com</a>)
+ */
+//-------------------------------------------------------------------------
+// Class fields
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+// Class functions
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+// Constructors
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+// Fields
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+// Methods
+//-------------------------------------------------------------------------
+public class BasicPropertyReaderTests extends TestCase {
+
+   /*
+    * @see TestCase#setUp()
+    */
+   protected void setUp()
+      throws Exception {
+      super.setUp();
+   }
+
+   /*
+    * @see TestCase#tearDown()
+    */
+   protected void tearDown()
+      throws Exception {
+      super.tearDown();
+   }
+
+   public void testBasicPropertyReader() {
+      //TODO Implement BasicPropertyReader().
+   }
+
+   public void testSet() {
+      //TODO Implement set().
+   }
+
+   public void testRemove() {
+      //TODO Implement remove().
+   }
+
+   public void testAbstractPropertyReader() {
+      //TODO Implement AbstractPropertyReader().
+   }
+
+   public void testGet() {
+      //TODO Implement get().
+   }
+
+   public void testGetNames() {
+      //TODO Implement getNames().
+   }
+
+   public void testGetPropertiesMap() {
+      //TODO Implement getPropertiesMap().
+   }
+
+}
