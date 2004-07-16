08f3c161367487f4bb08aacad7fbae70b409e7bd
chrisgilbride <>
2004-07-16 08:32:03 +0000

    Added tests for BasicPropertyReader

--- src/tests/org/xins/tests/common/collections/BasicPropertyReaderTests.java
+++ src/tests/org/xins/tests/common/collections/BasicPropertyReaderTests.java
@@ -3,30 +3,66 @@
  */
 package org.xins.tests.common.collections;
 
+import java.util.Properties;
+
+import org.apache.log4j.PropertyConfigurator;
+
+import junit.framework.Test;
 import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.xins.common.collections.BasicPropertyReader;
 
 /**
- * @TODO Fill in the class description here.
+ * Tests for class <code>BasicPropertyReader</code>.
  *
  * @version $Revision$
  * @author Mees Witteman (<a href="mailto:mees.witteman@nl.wanadoo.com">mees.witteman@nl.wanadoo.com</a>)
+ * @author Chris Gilbride (<a href="mailto:chris.gilbride@nl.wanadoo.com">Chris.gilbride@nl.wanadoo.com</a>)
  */
-//-------------------------------------------------------------------------
-// Class fields
-//-------------------------------------------------------------------------
-//-------------------------------------------------------------------------
-// Class functions
-//-------------------------------------------------------------------------
-//-------------------------------------------------------------------------
-// Constructors
-//-------------------------------------------------------------------------
-//-------------------------------------------------------------------------
-// Fields
-//-------------------------------------------------------------------------
-//-------------------------------------------------------------------------
-// Methods
-//-------------------------------------------------------------------------
 public class BasicPropertyReaderTests extends TestCase {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   /**
+    * Returns a test suite with all test cases defined by this class.
+    *
+    * @return
+    *    the test suite, never <code>null</code>.
+    */
+   public static Test suite() {
+      return new TestSuite(BasicPropertyReaderTests.class);
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>BasicPropertyReader</code> test suite with
+    * the specified name. The name will be passed to the superconstructor.
+    *
+    * @param name
+    *    the name for this test suite.
+    */
+   public BasicPropertyReaderTests(String name) {
+      super(name);
+   }
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
 
    /*
     * @see TestCase#setUp()
@@ -34,6 +70,14 @@ public class BasicPropertyReaderTests extends TestCase {
    protected void setUp()
       throws Exception {
       super.setUp();
+      Properties settings = new Properties();
+      settings.setProperty("log4j.rootLogger",                                "DEBUG, console");
+      settings.setProperty("log4j.appender.console",                          "org.apache.log4j.ConsoleAppender");
+      settings.setProperty("log4j.appender.console.layout",                   "org.apache.log4j.PatternLayout");
+      settings.setProperty("log4j.appender.console.layout.ConversionPattern", "%d %t %-5p [%c] %m%n");
+      settings.setProperty("log4j.logger.httpclient.wire",                    "WARN");
+      settings.setProperty("log4j.logger.org.apache.commons.httpclient",      "WARN");
+      PropertyConfigurator.configure(settings);
    }
 
    /*
@@ -44,31 +88,66 @@ public class BasicPropertyReaderTests extends TestCase {
       super.tearDown();
    }
 
-   public void testBasicPropertyReader() {
-      //TODO Implement BasicPropertyReader().
-   }
-
    public void testSet() {
-      //TODO Implement set().
+      BasicPropertyReader reader = new BasicPropertyReader();
+      try {
+         reader.set(null, null);
+         fail("BasicPropertyReader.set(null, null) should throw an IllegalArgumentException.");
+      } catch (IllegalArgumentException exception) {
+         // as expected
+      }
+
+      try {
+         reader.set(null, "test");
+         fail("BasicPropertyReader.set(null, \"test\") should throw an IllegalArgumentException.");
+      } catch (IllegalArgumentException exception) {
+         // as expected
+      }
+      
+      String testVal = "value";
+      String testName = "first";
+      // Check that the value testVal has been set in the BasicPropertyReader
+      reader.set(testName, testVal);
+      assertEquals(testVal, reader.get(testName));
    }
 
    public void testRemove() {
-      //TODO Implement remove().
-   }
+      BasicPropertyReader reader = new BasicPropertyReader();
+      try {
+         reader.remove(null);
+         fail("BasicPropertyReader.remove(null) should throw an IllegalArgumentException.");
+      } catch (IllegalArgumentException exception) {
+         // as expected
+      }
 
-   public void testAbstractPropertyReader() {
-      //TODO Implement AbstractPropertyReader().
+      // first add something in the property reader
+      String testVal = "value";
+      String testName = "first";
+      reader.set(testName, testVal);
+      // check it's there
+      assertEquals(testVal, reader.get(testName));
+      // now remove it
+      reader.remove(testName);
+      assertEquals(null, reader.get(testName));
    }
 
    public void testGet() {
-      //TODO Implement get().
+      BasicPropertyReader reader = new BasicPropertyReader();
+      try {
+         reader.get(null);
+         fail("BasicPropertyReader.get(null) should throw an IllegalArgumentException.");
+      } catch (IllegalArgumentException exception) {
+         // as expected
+      }
    }
 
    public void testGetNames() {
+      BasicPropertyReader reader = new BasicPropertyReader();
       //TODO Implement getNames().
    }
 
    public void testGetPropertiesMap() {
+      BasicPropertyReader reader = new BasicPropertyReader();
       //TODO Implement getPropertiesMap().
    }
 
