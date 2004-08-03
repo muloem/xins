7e78ce74a248ab21d2ceeecd4e04093948a938ef
znerd <>
2004-08-03 06:30:12 +0000

    Added constructor that accepts CallRequest and TargetDescriptor and deprecated old one.

--- src/java-client-framework/org/xins/client/CallIOException.java
+++ src/java-client-framework/org/xins/client/CallIOException.java
@@ -5,7 +5,8 @@ package org.xins.client;
 
 import java.io.IOException;
 
-import org.xins.common.ExceptionUtils;
+import org.xins.common.MandatoryArgumentChecker;
+import org.xins.common.service.TargetDescriptor;
 
 /**
  * Exception thrown to indicate that a XINS API call failed due to an I/O
@@ -45,10 +46,51 @@ extends CallException {
     *
     * @throws IllegalArgumentException
     *    if <code>ioException == null</code>.
+    *
+    * @deprecated
+    *    Deprecated since XINS 0.201. Use
+    *    {@link #CallIOException(CallRequest,TargetDescriptor,IOException)}
+    *    instead.
     */
    public CallIOException(IOException ioException)
    throws IllegalArgumentException {
-      super(ioException, null);
+
+      // Call superconstructor first
+      super(null, ioException);
+
+      // Check preconditions
+      MandatoryArgumentChecker.check("ioException", ioException);
+   }
+
+   /**
+    * Constructs a new <code>CallIOException</code> based on an
+    * <code>IOException</code>, for the specified <code>CallRequest</code> and
+    * <code>TargetDescriptor</code>.
+    *
+    * @param request
+    *    the original request, cannot be <code>null</code>.
+    *
+    * @param target
+    *    descriptor for the target that was attempted to be called, cannot be
+    *    <code>null</code>.
+    *
+    * @param ioException
+    *    the cause {@link IOException}, cannot be <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>ioException == null</code>.
+    */
+   public CallIOException(CallRequest      request,
+                          TargetDescriptor target,
+                          IOException      ioException)
+   throws IllegalArgumentException {
+
+      // Call superconstructor first
+      // TODO: super(request, target, checkArguments(request, target, ioException), null);
+      super(request, target, null, ioException);
+
+      // Check preconditions
+      MandatoryArgumentChecker.check("ioException", ioException);
    }
 
 
