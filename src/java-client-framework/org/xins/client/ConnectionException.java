36ccc3562caee6bfe2a74f2333514f534118c8fa
znerd <>
2004-08-03 07:17:21 +0000

    Added constructor that accepts CallRequest and TargetDescriptor. Deprecated old constructor.

--- src/java-client-framework/org/xins/client/ConnectionException.java
+++ src/java-client-framework/org/xins/client/ConnectionException.java
@@ -3,6 +3,8 @@
  */
 package org.xins.client;
 
+import org.xins.common.service.TargetDescriptor;
+
 /**
  * Exception that indicates that a connection could not be established.
  *
@@ -27,9 +29,35 @@ public abstract class ConnectionException extends CallException {
 
    /**
     * Constructs a new <code>ConnectionException</code>.
+    *
+    * @deprecated
+    *    Deprecated since XINS 0.201. Use
+    *    {@link #ConnectionException(CallRequest,TargetDescriptor)} instead.
     */
    ConnectionException() {
       super(null, null);
+   }
+
+   /**
+    * Constructs a new <code>ConnectionException</code>, for the specified
+    * <code>CallRequest</code> and <code>TargetDescriptor</code>.
+    *
+    * @param request
+    *    the original request, cannot be <code>null</code>.
+    *
+    * @param target
+    *    descriptor for the target that was attempted to be called, cannot be
+    *    <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>request == null || target == null</code>.
+    */
+   ConnectionException(CallRequest      request,
+                       TargetDescriptor target)
+   throws IllegalArgumentException {
+
+      // Call superconstructor first
+      super(request, target, null, null);
    }
 
 
