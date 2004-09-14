bb8c925215fa4f50f827cb88dd5242cc3175a927
znerd <>
2004-09-14 09:31:08 +0000

    Initial version.

---
+++ src/java-common/org/xins/common/service/TotalTimeOutCallException.java
@@ -0,0 +1,62 @@
+/*
+ * $Id$
+ */
+package org.xins.common.service;
+
+/**
+ * Exception that indicates the total time-out for a request was reached, so
+ * the request was aborted.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:ernst.dehaan@nl.wanadoo.com">ernst.dehaan@nl.wanadoo.com</a>)
+ *
+ * @since XINS 0.207
+ */
+public class TotalTimeOutCallException extends GenericCallException {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>TotalTimeOutCallException</code>.
+    *
+    * @param request
+    *    the original request, cannot be <code>null</code>.
+    *
+    * @param target
+    *    descriptor for the target that was attempted to be called, cannot be
+    *    <code>null</code>.
+    *
+    * @param duration
+    *    the call duration in milliseconds, must be &gt;= 0.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>request     == null
+    *          || target      == null
+    *          || duration  &lt; 0</code>.
+    */
+   TotalTimeOutCallException(CallRequest      request,
+                             TargetDescriptor target,
+                             long             duration)
+   throws IllegalArgumentException {
+      super("Total time-out", request, target, duration, null, null);
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+}
