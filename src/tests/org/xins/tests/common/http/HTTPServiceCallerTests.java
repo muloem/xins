f2e166ffa231505cf88cc1b7cf8359c70cd23f2b
znerd <>
2004-10-25 14:14:23 +0000

    Set all time-outs to 60 seconds. Allowing 0 duration.

--- src/tests/org/xins/tests/common/http/HTTPServiceCallerTests.java
+++ src/tests/org/xins/tests/common/http/HTTPServiceCallerTests.java
@@ -27,6 +27,7 @@ import org.xins.common.service.TargetDescriptor;
  *
  * @version $Revision$
  * @author Anthony Goubard (<a href="mailto:anthony.goubard@nl.wanadoo.com">anthony.goubard@nl.wanadoo.com</a>)
+ * @author Ernst de Haan (<a href="mailto:ernst.dehaan@nl.wanadoo.com">ernst.dehaan@nl.wanadoo.com</a>)
  */
 public class HTTPServiceCallerTests extends TestCase {
 
@@ -84,13 +85,15 @@ public class HTTPServiceCallerTests extends TestCase {
    //-------------------------------------------------------------------------
 
    public void testW3URL() throws Exception {
+      final int timeOut = 10000;
+
       HTTPCallRequest request = new HTTPCallRequest(HTTPMethod.GET);
-      Descriptor descriptor = new TargetDescriptor("http://www.w3.org/TR/2004/REC-xml-20040204/");
+      Descriptor descriptor = new TargetDescriptor("http://www.w3.org/TR/2004/REC-xml-20040204/", timeOut);
       HTTPServiceCaller caller = new HTTPServiceCaller(descriptor);
       HTTPCallResult result = caller.call(request);
       assertEquals("Received incorrect status code.", 200, result.getStatusCode());
       assertEquals("Incorrect succeeded descriptor.", descriptor, result.getSucceededTarget());
-      assertTrue("Incorrect duration.", result.getDuration() > 0 && result.getDuration() < 5000);
+      assertTrue("Incorrect duration.", result.getDuration() >= 0);
       String text = result.getString();
       boolean correctStart = text.startsWith("<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"><html lang=\"EN\" xmlns=\"http://www.w3.org/1999/xhtml\"><head><meta http-equiv=\"Content-Type\" content=\"text/html;charset=UTF-8\" /><title>Extensible Markup Language (XML) 1.0 (Third Edition)</title>");
       assertTrue("Unexpected HTML received.", correctStart);
@@ -98,61 +101,65 @@ public class HTTPServiceCallerTests extends TestCase {
    }
 
    public void testParameters() throws Exception {
+      final int timeOut = 60000;
       BasicPropertyReader parameters = new BasicPropertyReader();
       parameters.set("pattern", "^([A-Za-z]([A-Za-z\\- ]{0,26}[A-Za-z])?)$");
       parameters.set("string", "Janwillem");
       parameters.set("submit", "submit");
       // XXX GET method doesn't work
       HTTPCallRequest request = new HTTPCallRequest(HTTPMethod.POST, parameters);
-      Descriptor descriptor = new TargetDescriptor("http://xins.sourceforge.net/patterntest.php", 50000);
+      Descriptor descriptor = new TargetDescriptor("http://xins.sourceforge.net/patterntest.php", timeOut);
       HTTPServiceCaller caller = new HTTPServiceCaller(descriptor);
       HTTPCallResult result = caller.call(request);
       assertEquals("Received incorrect status code.", 200, result.getStatusCode());
       assertEquals("Incorrect succeeded descriptor.", descriptor, result.getSucceededTarget());
-      assertTrue("Incorrect duration.", result.getDuration() > 0 && result.getDuration() < 5000);
+      assertTrue("Incorrect duration.", result.getDuration() >= 0);
       String text = result.getString();
       assertTrue("Incorect content.", text.indexOf("\"Janwillem\" <span style='color:blue'>matches</span>") != -1);
    }
 
    public void testWrongURL() throws Exception {
+      final int timeOut = 60000;
       BasicPropertyReader parameters = new BasicPropertyReader();
       parameters.set("hello", "world");
       HTTPCallRequest request = new HTTPCallRequest(HTTPMethod.GET, parameters);
-      Descriptor descriptor = new TargetDescriptor("http://www.w3.org/nOnExIsTeNt.html");
+      Descriptor descriptor = new TargetDescriptor("http://www.w3.org/nOnExIsTeNt.html", timeOut);
       HTTPServiceCaller caller = new HTTPServiceCaller(descriptor);
 
       HTTPCallResult result = caller.call(request);
       assertEquals("Received incorrect status code.", result.getStatusCode(), 404);
-      assertTrue("Incorrect duration.", result.getDuration() > 0);
+      assertTrue("Incorrect duration.", result.getDuration() >= 0);
    }
    
    public void testFailOverGet() throws Exception {
+      final int timeOut = 60000;
       HTTPCallRequest request = new HTTPCallRequest(HTTPMethod.GET, null, false, null);
-      TargetDescriptor failedTarget = new TargetDescriptor("http://anthony.xins.org");
-      TargetDescriptor succeededTarget = new TargetDescriptor("http://www.w3.org/StyleSheets/TR/W3C-REC.css");
+      TargetDescriptor failedTarget = new TargetDescriptor("http://anthony.xins.org", timeOut);
+      TargetDescriptor succeededTarget = new TargetDescriptor("http://www.w3.org/StyleSheets/TR/W3C-REC.css", timeOut);
       TargetDescriptor[] descriptors = {failedTarget, succeededTarget};
       GroupDescriptor descriptor = new GroupDescriptor(GroupDescriptor.ORDERED_TYPE, descriptors);
       HTTPServiceCaller caller = new HTTPServiceCaller(descriptor);
 
       HTTPCallResult result = caller.call(request);
       assertEquals("Received incorrect status code.", 200, result.getStatusCode());
-      assertTrue("Incorrect duration.", result.getDuration() > 0);
+      assertTrue("Incorrect duration.", result.getDuration() >= 0);
       assertEquals("Incorrect succeeded target.", succeededTarget, result.getSucceededTarget());
       String text = result.getString();
       assertTrue("Incorrect content.", text.indexOf("Copyright 1997-") > 0);
    }
    
    public void testFailOverPost() throws Exception {
+      final int timeOut = 60000;
       HTTPCallRequest request = new HTTPCallRequest(HTTPMethod.POST, null, true, null);
-      TargetDescriptor failedTarget = new TargetDescriptor("http://anthony.xins.org");
-      TargetDescriptor succeededTarget = new TargetDescriptor("http://xins.sourceforge.net/patterntest.php", 50000);
+      TargetDescriptor failedTarget = new TargetDescriptor("http://anthony.xins.org", timeOut);
+      TargetDescriptor succeededTarget = new TargetDescriptor("http://xins.sourceforge.net/patterntest.php", timeOut);
       TargetDescriptor[] descriptors = {failedTarget, succeededTarget};
       GroupDescriptor descriptor = new GroupDescriptor(GroupDescriptor.ORDERED_TYPE, descriptors);
       HTTPServiceCaller caller = new HTTPServiceCaller(descriptor);
 
       HTTPCallResult result = caller.call(request);
       assertEquals("Received incorrect status code.", 200, result.getStatusCode());
-      assertTrue("Incorrect duration.", result.getDuration() > 0);
+      assertTrue("Incorrect duration.", result.getDuration() >= 0);
       assertEquals("Incorrect succeeded target.", succeededTarget, result.getSucceededTarget());
       String text = result.getString();
       assertTrue("Incorrect content.", text.indexOf("Pattern test form") != -1);
