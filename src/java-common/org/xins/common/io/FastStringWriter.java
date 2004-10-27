f960f782466b468b2dace09e09b17b032a0c7777
znerd <>
2004-10-27 10:21:57 +0000

    Improved docs.

--- src/java-common/org/xins/common/io/FastStringWriter.java
+++ src/java-common/org/xins/common/io/FastStringWriter.java
@@ -13,6 +13,8 @@ import org.xins.common.text.FastStringBuffer;
  * implements a character stream that collects its output in a fast,
  * unsynchronized string buffer, which can then be used to construct a string.
  *
+ * <p>Instances of this class are not thread-safe.
+ *
  * @version $Revision$ $Date$
  * @author Ernst de Haan (<a href="mailto:ernst.dehaan@nl.wanadoo.com">ernst.dehaan@nl.wanadoo.com</a>)
  */
@@ -22,6 +24,9 @@ public class FastStringWriter extends Writer {
    // Class fields
    //-------------------------------------------------------------------------
 
+   /**
+    * The default initial internal buffer size.
+    */
    private final static int DEFAULT_INITIAL_SIZE = 128;
 
 
@@ -35,7 +40,7 @@ public class FastStringWriter extends Writer {
 
    /**
     * Creates a new <code>FastStringWriter</code> using a default initial
-    * string buffer size.
+    * internal buffer size.
     */
    public FastStringWriter() {
       _buffer = new FastStringBuffer(DEFAULT_INITIAL_SIZE);
@@ -53,9 +58,13 @@ public class FastStringWriter extends Writer {
     */
    public FastStringWriter(int initialSize)
    throws IllegalArgumentException {
+
+      // Check preconditions
       if (initialSize < 0) {
          throw new IllegalArgumentException("initialSize (" + initialSize + ") < 0");
       }
+
+      // Initialize internal buffer
       _buffer = new FastStringBuffer(initialSize);
    }
 
@@ -92,31 +101,111 @@ public class FastStringWriter extends Writer {
       }
    }
 
+   /**
+    * Writes a single character.
+    *
+    * @param c
+    *    the character to write.
+    *
+    * @throws IOException
+    *    if this writer has been closed (see {@link #close()}).
+    */
    public void write(int c) throws IOException {
       assertOpen();
       _buffer.append((char) c);
    }
 
-   public void write(char cbuf[]) throws IOException {
+   /**
+    * Writes an array of characters.
+    *
+    * @param cbuf
+    *    the array of characters to write, cannot be <code>null</code>.
+    *
+    * @throws IOException
+    *    if this writer has been closed (see {@link #close()}).
+    *
+    * @throws IllegalArgumentException
+    *    if <code>cbuf == null</code>.
+    */
+   public void write(char cbuf[])
+   throws IOException, IllegalArgumentException {
       assertOpen();
       MandatoryArgumentChecker.check("cbuf", cbuf);
       _buffer.append(cbuf);
    }
 
+   /**
+    * Writes a portion of an array of characters.
+    *
+    * @param cbuf
+    *    the array of characters to write a portion of, cannot be
+    *    <code>null</code>.
+    *
+    * @param off
+    *    offset from which to start writing characters, must be &gt;= 0 and
+    *    &lt; <code>sbuf.length</code>.
+    *
+    * @param len
+    *    the number of characters to write.
+    *
+    * @throws IOException
+    *    if this writer has been closed (see {@link #close()}).
+    *
+    * @throws IllegalArgumentException
+    *    if <code>cbuf == null</code>.
+    *
+    * @throws IndexOutOfBoundsException
+    *    if the offset and/or the length is invalid.
+    */
    public void write(char cbuf[], int off, int len)
-   throws IllegalArgumentException, IndexOutOfBoundsException, IOException {
+   throws IllegalArgumentException, IOException, IndexOutOfBoundsException {
       assertOpen();
       MandatoryArgumentChecker.check("cbuf", cbuf);
       _buffer.append(cbuf, off, len);
    }
 
+   /**
+    * Writes a character string.
+    *
+    * @param str
+    *    the character string to write, cannot be <code>null</code>.
+    *
+    * @throws IOException
+    *    if this writer has been closed (see {@link #close()}).
+    *
+    * @throws IllegalArgumentException
+    *    if <code>str == null</code>.
+    */
    public void write(String str)
-   throws IllegalArgumentException, IOException {
+   throws IOException, IllegalArgumentException {
       assertOpen();
       MandatoryArgumentChecker.check("str", str);
       _buffer.append(str);
    }
 
+   /**
+    * Writes a portion of a character string.
+    *
+    * @param str
+    *    the character string to write a portion of, cannot be
+    *    <code>null</code>.
+    *
+    * @param off
+    *    offset from which to start writing characters, must be &gt;= 0 and
+    *    &lt; <code>str.{@link String#length() length()}</code>.
+    *
+    * @param len
+    *    the number of characters to write.
+    *
+    * @throws IOException
+    *    if this writer has been closed (see {@link #close()}).
+    *
+    * @throws IllegalArgumentException
+    *    if <code>str == null</code>.
+    *
+    * @throws IndexOutOfBoundsException
+    *    if the offset and/or the length is invalid.
+    */
    public void write(String str, int off, int len)
    throws IllegalArgumentException, IOException {
       assertOpen();
@@ -124,10 +213,24 @@ public class FastStringWriter extends Writer {
       _buffer.append(str.substring(off, off + len));
    }
 
+   /**
+    * Flushes this writer.
+    *
+    * <p>The implementation of this method does not nothing except checking
+    * that this writer is not yet closed.
+    *
+    * @throws IOException
+    *    if this writer has been closed (see {@link #close()}).
+    */
    public void flush() throws IOException {
       assertOpen();
    }
 
+   /**
+    * Closes this writer. If this writer was already closed, then nothing
+    * happens. After calling this method, the write and flush methods will
+    * throw an {@link IOException} if called.
+    */
    public void close() {
       _closed = true;
    }
