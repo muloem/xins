1c99c547d73cac0655f33d38d531acd6fe8df2a7
znerd <>
2004-11-23 13:34:31 +0000

    Initial version.

---
+++ src/java-client-framework/org/xins/client/AbstractCAPICallRequest.java
@@ -0,0 +1,47 @@
+/*
+ * $Id$
+ *
+ * Copyright 2004 Wanadoo Nederland B.V.
+ * See the COPYRIGHT file for redistribution and use restrictions.
+ */
+package org.xins.client;
+
+/**
+ * Base class for CAPI call request classes.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:ernst.dehaan@nl.wanadoo.com">ernst.dehaan@nl.wanadoo.com</a>)
+ *
+ * @since XINS 1.1.0
+ */
+public abstract class AbstractCAPICallRequest
+extends Object {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Creates a new <code>AbstractCAPICallRequest</code> object.
+    */
+   protected AbstractCAPICallRequest() {
+      // empty
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+}
