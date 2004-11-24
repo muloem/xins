e6de364fd2494d3f1a83b8e8771cfd36ff0c4a39
znerd <>
2004-11-24 22:57:36 +0000

    Added failOverAllowed property.

--- src/java-common/org/xins/common/service/CallConfig.java
+++ src/java-common/org/xins/common/service/CallConfig.java
@@ -7,12 +7,23 @@
 package org.xins.common.service;
 
 import org.xins.common.Log;
+import org.xins.common.threads.Doorman;
 
 /**
  * Abstraction of a call configuration. Objects of this type specify certain
  * aspects of <em>how</em> a call is executed. For example, for an HTTP
  * service caller, a <code>CallConfig</code> object could specify what HTTP
  * method (GET, POST, etc.) to use.
+ *
+ * <p>This base class only specifies the property <em>failOverAllowed</em>,
+ * which indicates whether fail-over is unconditionally allowed, even if the
+ * request was already received or even processed by the other end.
+ *
+ * <p>This class is thread-safe.
+ *
+ * <p>Note to implementors of this class: Subclasses <em>must</em> be
+ * thread-safe as well. They should use {@link #_doorman} to synchronize the
+ * read and write operations to all fields.
  *
  * @version $Revision$ $Date$
  * @author Ernst de Haan (<a href="mailto:ernst.dehaan@nl.wanadoo.com">ernst.dehaan@nl.wanadoo.com</a>)
@@ -48,7 +59,8 @@ public abstract class CallConfig extends Object {
       // TRACE: Enter constructor
       Log.log_1000(CLASSNAME, null);
 
-      // empty
+      // TODO: Improve name sent to Doorman constructor
+      _doorman = new Doorman("CallConfig", false, 3, 4000);
 
       // TRACE: Leave constructor
       Log.log_1002(CLASSNAME, null);
@@ -59,7 +71,55 @@ public abstract class CallConfig extends Object {
    // Fields
    //-------------------------------------------------------------------------
 
+   /**
+    * Access controller for the fields in this object.
+    *
+    * <p>When reading a field, {@link Doorman#enterAsReader() enterAsReader()}
+    * should be called, followed by
+    * {@link Doorman#leaveAsReader() leaveAsReader()} when done.
+    *
+    * <p>When writing a field, {@link Doorman#enterAsWriter() enterAsWriter()}
+    * should be called, followed by
+    * {@link Doorman#leaveAsWriter() leaveAsWriter()} when done.
+    */
+   protected final Doorman _doorman;
+
+   /**
+    * Flag that indicates whether fail-over is unconditionally allowed.
+    */
+   private boolean _failOverAllowed;
+
+
    //-------------------------------------------------------------------------
    // Methods
    //-------------------------------------------------------------------------
+
+   /**
+    * Determines whether fail-over is unconditionally allowed.
+    *
+    * @return
+    *    <code>true</code> if fail-over is unconditionally allowed, even if
+    *    the request was already received or even processed by the other end,
+    *    <code>false</code> otherwise.
+    */
+   public final boolean isFailOverAllowed() {
+      _doorman.enterAsReader();
+      boolean b = _failOverAllowed;
+      _doorman.leaveAsReader();
+      return b;
+   }
+
+   /**
+    * Configures whether fail-over is unconditionally allowed.
+    *
+    * @param allowed
+    *    <code>true</code> if fail-over is unconditionally allowed, even if
+    *    the request was already received or even processed by the other end,
+    *    <code>false</code> otherwise.
+    */
+   public final void setFailOverAllowed(boolean allowed) {
+      _doorman.enterAsWriter();
+      _failOverAllowed = allowed;
+      _doorman.leaveAsWriter();
+   }
 }
