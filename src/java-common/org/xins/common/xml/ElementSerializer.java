668a9f7b1d8d077341070ae00682072dd920ef89
znerd <>
2004-11-26 15:39:03 +0000

    Made constructor public. Changed serialize method to non-static. Added namespaces-support.

--- src/java-common/org/xins/common/xml/ElementSerializer.java
+++ src/java-common/org/xins/common/xml/ElementSerializer.java
@@ -8,22 +8,32 @@ package org.xins.common.xml;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
+import org.xins.common.Log;
 import org.xins.common.MandatoryArgumentChecker;
 import org.xins.common.io.FastStringWriter;
+
 import org.znerd.xmlenc.XMLOutputter;
 
 /**
- * Convert the Element to XML.
+ * Serializer that takes an <code>Element</code> and converts it to an XML
+ * string.
+ *
+ * <p>This class is not thread-safe. It should only be used on one thread at a
+ * time.
  *
  * @version $Revision$ $Date$
  * @author Anthony Goubard (<a href="mailto:anthony.goubard@nl.wanadoo.com">anthony.goubard@nl.wanadoo.com</a>)
+ * @author Ernst de Haan (<a href="mailto:ernst.dehaan@nl.wanadoo.com">ernst.dehaan@nl.wanadoo.com</a>)
  *
  * @since XINS 1.1.0
  */
-public class ElementSerializer {
+public final class ElementSerializer extends Object {
    
    //-------------------------------------------------------------------------
    // Class fields
@@ -34,8 +44,49 @@ public class ElementSerializer {
     */
    private static final String CLASSNAME = ElementSerializer.class.getName();
 
+
    //-------------------------------------------------------------------------
    // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>ElementSerializer</code>.
+    */
+   public ElementSerializer() {
+      _lock       = new Object();
+      _namespaces = new HashMap();
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   /**
+    * Lock object that is synchronized on when reading or writing
+    * <code>_inUse</code>.
+    */
+   private final Object _lock;
+
+   /**
+    * Flag that indicates whether this serializer is currently in use. It may
+    * only be used by one thread at a time.
+    */
+   private boolean _inUse;
+
+   /**
+    * Map that links namespaces to their prefixes. Each key is a namespace and
+    * the corresponding value is the associated prefix.
+    */
+   private Map _namespaces;
+
+
+   //-------------------------------------------------------------------------
+   // Methods
    //-------------------------------------------------------------------------
 
    /**
@@ -44,37 +95,79 @@ public class ElementSerializer {
     * @param element
     *    the element to serialize, cannot be <code>null</code>.
     *
+    * @return
+    *    an XML document that is represents <code>element</code>, never
+    *    <code>null</code>.
+    *
+    * @throws IllegalStateException
+    *    if this serializer is already in use.
+    *
     * @throws IllegalArgumentException
     *    if <code>element == null</code>.
-    *
-    * @throws IOException
-    *    if there is an I/O error.
     */
-   public static String serialize(Element element)
-   throws IllegalArgumentException, IOException {
+   public String serialize(Element element)
+   throws IllegalStateException, IllegalArgumentException {
 
-      // Check preconditions
+      // TODO: TRACE logging
+
+      synchronized (_lock) {
+
+         // Make sure this serializer is not yet in use
+         if (_inUse) {
+            // TODO: Log programming error
+            throw new IllegalStateException("This ElementSerializer is currently already in use.");
+         }
+
+         // Lock this serializer
+         _inUse = true;
+      }
+
+      // Check argument
       MandatoryArgumentChecker.check("element", element);
 
       // Create an XMLOutputter
-      FastStringWriter out = new FastStringWriter();
-      XMLOutputter xmlout = new XMLOutputter(out, "UTF-8");
+      FastStringWriter fsw = new FastStringWriter();
+      XMLOutputter out;
+      final String ENCODING = "UTF-8";
+      try {
+         out = new XMLOutputter(fsw, "UTF-8");
+      } catch (UnsupportedEncodingException uee) {
+         Log.log_1052(uee, XMLOutputter.class.getName(), "<init>(Writer,String)");
+         throw new Error(uee); // TODO
+      }
 
-      // Output the declaration
-      // XXX: Make it configurable whether the declaration is output or not?
-      xmlout.declaration();
+      // XXX: Allow output of declaration to be configured?
 
-      output(xmlout, element);
+      // Output the XML that represents the Element
+      try {
+         output(out, element);
 
-      out.close();
+      // I/O errors should not happen on a FastStringWriter
+      } catch (IOException exception) {
+         Log.log_1052(exception, CLASSNAME, "output(XMLOutputter,Element)");
+         throw new Error(CLASSNAME + ".output(XMLOutputter,Element) threw unexpected " + exception.getClass().getName() + '.', exception);
 
-      return out.toString();
+      // Always close the FastStringWriter
+      } finally {
+         fsw.close();
+      }
+
+      String xml = out.toString();
+
+      // Unlock this serializer
+      synchronized (_lock) {
+         _inUse = false;
+      }
+
+      // TODO: TRACE logging
+
+      return xml;
    }
 
    /**
-    * Generates XML for the specified element.
+    * Generates XML for the specified <code>Element</code>.
     *
-    * @param xmlout
+    * @param out
     *    the {@link XMLOutputter} to use, cannot be <code>null</code>.
     *
     * @param element
@@ -82,49 +175,99 @@ public class ElementSerializer {
     *    <code>null</code>.
     *
     * @throws NullPointerException
-    *    if <code>xmlout == null || element == null</code>.
+    *    if <code>out == null || element == null</code>.
     *
     * @throws IOException
     *    if there is an I/O error.
     */
-   private static final void output(XMLOutputter xmlout, Element element)
+   private void output(XMLOutputter out, Element element)
    throws NullPointerException, IOException {
 
-      // Start the tag
-      xmlout.startTag(element.getLocalName());
+      String namespaceURI = element.getNamespaceURI();
+      String localName    = element.getLocalName();
 
-      // Write the attributes
+      // Write an element with namespace
+      if (namespaceURI != null) {
+
+         // Determine if the namespace already has an assigned prefix
+         String prefix = (String) _namespaces.get(namespaceURI);
+         boolean newNamespace = (prefix == null);
+         if (newNamespace) {
+
+            // Generate a namespace prefix
+            prefix = "ns" + _namespaces.size();
+
+            // Remember the association of the namespace with the prefix
+            _namespaces.put(namespaceURI, prefix);
+         }
+
+         // Write the start tag with prefix
+         out.startTag(prefix + ':' + localName);
+
+         // Associate the namespace with the prefix in the result XML
+         if (newNamespace) {
+            out.attribute("xmlns:" + prefix, namespaceURI);
+         }
+
+      // Write an element without namespace
+      } else {
+         out.startTag(localName);
+      }
+
+      // Loop through all attributes
       Map attributes = element.getAttributeMap();
       Iterator names = attributes.keySet().iterator();
       while (names.hasNext()) {
-         Element.QualifiedName qname  = (Element.QualifiedName) names.next();
-         String name = qname.getLocalName();
-         String value = (String) attributes.get(qname);
-         xmlout.attribute(name, value);
+
+         // Get the namespace, local name and value
+         Element.QualifiedName qn  = (Element.QualifiedName) names.next();
+         String attrNamespaceURI  = qn.getNamespaceURI();
+         String attrLocalName     = qn.getLocalName();
+         String attrValue         = (String) attributes.get(qn);
+
+         // Write an attribute with namespace
+         if (attrNamespaceURI != null) {
+
+            // Determine if the namespace already has an assigned prefix
+            String prefix = (String) _namespaces.get(attrNamespaceURI);
+            boolean newNamespace = (prefix == null);
+            if (newNamespace) {
+
+               // Generate a namespace prefix
+               prefix = "ns" + _namespaces.size();
+
+               // Remember the association of the namespace with the prefix
+               _namespaces.put(namespaceURI, prefix);
+            }
+
+            // Write the attribute with prefix
+            out.attribute(prefix + ':' + attrLocalName, attrValue);
+
+            // Associate the namespace with the prefix in the result XML
+            if (newNamespace) {
+               out.attribute("xmlns:" + prefix, attrNamespaceURI);
+            }
+
+         // Write an attribute without namespace
+         } else {
+            out.attribute(attrLocalName, attrValue);
+         }
       }
 
-      // Process all contained elements and PCDATA sections
+      // Process all contained elements
       List content = element.getChildElements();
       int count = content == null ? 0 : content.size();
       for (int i = 0; i < count; i++) {
          Object o = content.get(i);
-         output(xmlout, (Element) o);
+         output(out, (Element) o);
       }
+
+      // Output contained PCDATA
       if (element.getText() != null) {
-         xmlout.pcdata(element.getText());
+         out.pcdata(element.getText());
       }
 
       // End the tag
-      xmlout.endTag();
-   }
-
-   //-------------------------------------------------------------------------
-   // Constructors
-   //-------------------------------------------------------------------------
-
-   /**
-    * This class only contains static method.
-    */
-   private ElementSerializer() {
+      out.endTag();
    }
 }
