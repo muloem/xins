848126039e06718dd710a74fb2010e625f57e09e
znerd <>
2004-11-29 14:49:15 +0000

    - Replaced constructor that accepts XINSServiceCaller with constructor that   accepts Descriptor and XINSCallConfig. This constructor may throw an   UnsupportedProtocolException. - Changed constructor that accepts a Descriptor only. It may now also throw an   UnsupportedProtocolException.

--- src/java-client-framework/org/xins/client/AbstractCAPI.java
+++ src/java-client-framework/org/xins/client/AbstractCAPI.java
@@ -7,9 +7,13 @@
 package org.xins.client;
 
 import org.xins.common.MandatoryArgumentChecker;
+
 import org.xins.common.collections.PropertyReader;
+
 import org.xins.common.http.HTTPMethod;
+
 import org.xins.common.service.Descriptor;
+import org.xins.common.service.UnsupportedProtocolException;
 
 /**
  * Base class for client-side calling interface classes.
@@ -44,29 +48,32 @@ public abstract class AbstractCAPI extends Object {
     * <p>This constructor is considered internal to XINS. Do not use it
     * directly.
     *
-    * @param caller
-    *    the XINS service caller to use, cannot be <code>null</code>.
+    * @param descriptor
+    *    the descriptor for the service(s), cannot be <code>null</code>.
     *
     * @throws IllegalArgumentException
-    *    if <code>caller == null</code>.
+    *    if <code>descriptor == null</code>.
+    *
+    * @throws UnsupportedProtocolException
+    *    if any of the target descriptors specifies an unsupported protocol.
     *
     * @since XINS 1.1.0
     */
-   protected AbstractCAPI(XINSServiceCaller caller)
-   throws IllegalArgumentException {
+   protected AbstractCAPI(Descriptor descriptor, XINSCallConfig callConfig)
+   throws IllegalArgumentException, UnsupportedProtocolException {
 
       // Check preconditions
-      MandatoryArgumentChecker.check("caller", caller);
+      MandatoryArgumentChecker.check("descriptor", descriptor);
 
-      // Store service caller
-      _caller = caller;
+      // Create and store service caller
+      _caller = new XINSServiceCaller(descriptor, callConfig);
    }
 
    /**
     * Creates a new <code>AbstractCAPI</code> object, using the specified
     * <code>Descriptor</code>.
     *
-    * <p>The default HTTP method will be used when doing a call.
+    * <p>A default XINS call configuration will be used.
     *
     * <p>This constructor is considered internal to XINS. Do not use it
     * directly.
@@ -77,19 +84,18 @@ public abstract class AbstractCAPI extends Object {
     * @throws IllegalArgumentException
     *    if <code>descriptor == null</code>.
     *
+    * @throws UnsupportedProtocolException
+    *    if any of the target descriptors specifies an unsupported protocol
+    *    (<em>since XINS 1.1.0</em>).
+    *
     * @deprecated
     *    Deprecated since XINS 1.1.0. This constructor is expected to be
     *    removed in a later version of XINS. It is not removed yet to remain
     *    fully compatible with XINS 1.0.
     */
    protected AbstractCAPI(Descriptor descriptor)
-   throws IllegalArgumentException {
-
-      // Check preconditions
-      MandatoryArgumentChecker.check("descriptor", descriptor);
-
-      // Store service caller
-      _caller = new XINSServiceCaller(descriptor);
+   throws IllegalArgumentException, UnsupportedProtocolException {
+      this(descriptor, null);
    }
 
 
@@ -121,6 +127,7 @@ public abstract class AbstractCAPI extends Object {
     *    the XINS service caller to use, never <code>null</code>.
     */
    protected XINSServiceCaller getCaller() {
+      // TODO for XINS 2.0.0: Mark this method as final
       return _caller;
    }
 
