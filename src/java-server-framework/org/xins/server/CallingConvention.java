8bbe9d267d0527da3febbae06bbfb6c384db9137
znerd <>
2004-12-14 12:03:22 +0000

    Improved description.

--- src/java-server-framework/org/xins/server/CallingConvention.java
+++ src/java-server-framework/org/xins/server/CallingConvention.java
@@ -14,7 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.xins.common.text.ParseException;
 
 /**
- * The calling convention interface.
+ * Abstraction of a calling convention. A calling convention determines how an
+ * HTTP request is converted to a XINS request and how a XINS response is
+ * converted back to an HTTP response.
  *
  * @version $Revision$ $Date$
  * @author Anthony Goubard (<a href="mailto:anthony.goubard@nl.wanadoo.com">anthony.goubard@nl.wanadoo.com</a>)
@@ -24,24 +26,40 @@ import org.xins.common.text.ParseException;
 public interface CallingConvention {
    
    /**
-    * Gets the function request based on the query passed to the Servlet.
+    * Converts an HTTP request to a XINS request.
     *
     * @param request
-    *    the Servlet request.
+    *    the HTTP request, cannot be <code>null</code>.
+    *
+    * @return
+    *    the XINS request, never <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>request == null</code>.
     *
     * @throws ParseException
-    *    a ParseException is thrown if the request is not in the expected format.
+    *    if the request is considerd to be invalid.
     */
-   FunctionRequest getFunctionRequest(HttpServletRequest request) throws ParseException;
+   FunctionRequest getFunctionRequest(HttpServletRequest request)
+   throws IllegalArgumentException, ParseException;
+   // TODO: Replace ParseException with more appropriate exception
    
    /**
-    * Returns the function result in the expected format.
+    * Converts a XINS response to an HTTP response.
     *
     * @param response
-    *    the Servlet response where the response should be send.
+    *    the HTTP response object to configure, cannot be <code>null</code>.
+    *
     * @param result
-    *    the function result.
+    *    the XINS result object that should be converted to an HTTP response,
+    *    cannot be <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>response == null || result == null</code>.
     */
-   void handleResult(HttpServletResponse response, FunctionResult result) throws IOException;
-   
+   void handleResult(HttpServletResponse response, FunctionResult result)
+   throws IOException;
+   // TODO: Inverse the order of the arguments
+   // TODO: Improve the naming of the arguments
+   // TODO: Replace IOException with more appropriate exception
 }
