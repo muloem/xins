10075157537d8ea90bc6588ac9a5f53a2b6429b0
znerd <>
2004-12-27 15:38:37 +0000

    Initial version.

---
+++ src/tests/org/xins/tests/common/UtilsTests.java
@@ -0,0 +1,78 @@
+/*
+ * $Id$
+ *
+ * Copyright 2004 Wanadoo Nederland B.V.
+ * See the COPYRIGHT file for redistribution and use restrictions.
+ */
+package org.xins.tests.common;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.xins.common.Utils;
+
+/**
+ * Tests for class <code>Utils</code>
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:ernst.dehaan@nl.wanadoo.com">ernst.dehaan@nl.wanadoo.com</a>)
+ */
+public class UtilsTests extends TestCase {
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   /**
+    * Returns a test suite with all test cases defined by this class.
+    *
+    * @return
+    *    the test suite, never <code>null</code>.
+    */
+   public static Test suite() {
+      return new TestSuite(UtilsTests.class);
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructor
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>UtilsTests</code> test suite with
+    * the specified name. The name will be passed to the superconstructor.
+    *
+    * @param name
+    *    the name for this test suite.
+    */
+   public UtilsTests(String name) {
+      super(name);
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+
+   public void testGetCallingClassAndMethod() {
+      doTestGetCallingClassAndMethod(getClass().getName(), "testGetCallingClassAndMethod");
+   }
+
+   private void doTestGetCallingClassAndMethod(String expectedClass,
+                                               String expectedMethod) {
+      String c = Utils.getCallingClass();
+      String m = Utils.getCallingMethod();
+
+      assertEquals(expectedClass, c);
+      assertEquals(expectedMethod, m);
+   }
+}
