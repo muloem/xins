ff94068759736cca3e4f876d1cefed31e2b64416
znerd <>
2004-12-30 10:33:00 +0000

    Added PropertyReaderUtilsTests.

--- src/tests/org/xins/tests/AllTests.java
+++ src/tests/org/xins/tests/AllTests.java
@@ -36,6 +36,7 @@ public class AllTests extends TestSuite {
       suite.addTestSuite(org.xins.tests.common.UtilsTests.class);
 
       suite.addTestSuite(org.xins.tests.common.collections.BasicPropertyReaderTests.class);
+      suite.addTestSuite(org.xins.tests.common.collections.PropertyReaderUtilsTests.class);
       suite.addTestSuite(org.xins.tests.common.collections.expiry.ExpiryFolderTests.class);
       suite.addTestSuite(org.xins.tests.common.collections.expiry.ExpiryStrategyTests.class);
 
