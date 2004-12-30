11d134279ad6e3be9cdb91df5edb675d12aeb992
znerd <>
2004-12-30 11:11:17 +0000

    Added some more tests.

--- src/tests/org/xins/tests/common/collections/PropertyReaderUtilsTests.java
+++ src/tests/org/xins/tests/common/collections/PropertyReaderUtilsTests.java
@@ -6,7 +6,9 @@
  */
 package org.xins.tests.common.collections;
 
-import java.lang.Exception;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import java.util.Iterator;
 import java.util.Properties;
 
@@ -16,6 +18,7 @@ import junit.framework.TestSuite;
 
 import org.xins.common.collections.BasicPropertyReader;
 import org.xins.common.collections.InvalidPropertyValueException;
+import org.xins.common.collections.MissingRequiredPropertyException;
 import org.xins.common.collections.PropertyReader;
 import org.xins.common.collections.PropertyReaderUtils;
 
@@ -164,5 +167,179 @@ public class PropertyReaderUtilsTests extends TestCase {
       } catch (InvalidPropertyValueException exception) {
          // as expected
       }
+   }
+
+   public void testPropertyReaderUtils_getIntProperty()
+   throws Exception {
+
+      BasicPropertyReader r1 = new BasicPropertyReader();
+      PropertyReader      r0 = r1;
+
+      try {
+         PropertyReaderUtils.getIntProperty(null, null);
+         fail("Expected IllegalArgumentException.");
+      } catch (IllegalArgumentException exception) {
+         // as expected
+      }
+
+      try {
+         PropertyReaderUtils.getIntProperty(r0, null);
+         fail("Expected IllegalArgumentException.");
+      } catch (IllegalArgumentException exception) {
+         // as expected
+      }
+
+      try {
+         PropertyReaderUtils.getIntProperty(null, "propertyName");
+         fail("Expected IllegalArgumentException.");
+      } catch (IllegalArgumentException exception) {
+         // as expected
+      }
+
+      final long MIN_VALUE           = (long) Integer.MIN_VALUE;
+      final long MAX_VALUE           = (long) Integer.MAX_VALUE;
+      final long LESS_THAN_MIN_VALUE = MIN_VALUE - 1L;
+      final long MORE_THAN_MAX_VALUE = MAX_VALUE + 1L;
+
+      r1.set("a", null);
+      r1.set("b", "");
+      r1.set("c", "-1");
+      r1.set("d", "0");
+      r1.set("e", "1");
+      r1.set("f", Long.toString(MIN_VALUE));
+      r1.set("g", Long.toString(MAX_VALUE));
+      r1.set("h", Long.toString(LESS_THAN_MIN_VALUE));
+      r1.set("i", Long.toString(MORE_THAN_MAX_VALUE));
+      r1.set("j", "a");
+
+      try {
+         PropertyReaderUtils.getIntProperty(r0, "unsetProperty");
+         fail("Expected MissingRequiredPropertyException.");
+      } catch (MissingRequiredPropertyException exception) {
+         // as expected
+      }
+
+      try {
+         PropertyReaderUtils.getIntProperty(r0, "a");
+         fail("Expected MissingRequiredPropertyException.");
+      } catch (MissingRequiredPropertyException exception) {
+         // as expected
+      }
+
+      try {
+         PropertyReaderUtils.getIntProperty(r0, "b");
+         fail("Expected MissingRequiredPropertyException.");
+      } catch (MissingRequiredPropertyException exception) {
+         // as expected
+      }
+
+      assertEquals(-1, PropertyReaderUtils.getIntProperty(r0, "c"));
+      assertEquals(0,  PropertyReaderUtils.getIntProperty(r0, "d"));
+      assertEquals(1,  PropertyReaderUtils.getIntProperty(r0, "e"));
+      assertEquals(Integer.MIN_VALUE, PropertyReaderUtils.getIntProperty(r0, "f"));
+      assertEquals(Integer.MAX_VALUE, PropertyReaderUtils.getIntProperty(r0, "g"));
+
+      try {
+         PropertyReaderUtils.getIntProperty(r0, "h");
+         fail("Expected InvalidPropertyValueException.");
+      } catch (InvalidPropertyValueException exception) {
+         // as expected
+      }
+
+      try {
+         PropertyReaderUtils.getIntProperty(r0, "i");
+         fail("Expected InvalidPropertyValueException.");
+      } catch (InvalidPropertyValueException exception) {
+         // as expected
+      }
+
+      try {
+         PropertyReaderUtils.getIntProperty(r0, "j");
+         fail("Expected InvalidPropertyValueException.");
+      } catch (InvalidPropertyValueException exception) {
+         // as expected
+      }
+   }
+
+   public void testPropertyReaderUtils_getRequiredProperty()
+   throws Exception {
+
+      BasicPropertyReader r1 = new BasicPropertyReader();
+      PropertyReader      r0 = r1;
+
+      try {
+         PropertyReaderUtils.getRequiredProperty(null, null);
+         fail("Expected IllegalArgumentException.");
+      } catch (IllegalArgumentException exception) {
+         // as expected
+      }
+
+      try {
+         PropertyReaderUtils.getRequiredProperty(r0, null);
+         fail("Expected IllegalArgumentException.");
+      } catch (IllegalArgumentException exception) {
+         // as expected
+      }
+
+      try {
+         PropertyReaderUtils.getRequiredProperty(null, "propertyName");
+         fail("Expected IllegalArgumentException.");
+      } catch (IllegalArgumentException exception) {
+         // as expected
+      }
+
+      r1.set("a", null);
+      r1.set("b", "");
+      r1.set("c", "value");
+
+      try {
+         PropertyReaderUtils.getRequiredProperty(r0, "unsetProperty");
+         fail("Expected MissingRequiredPropertyException.");
+      } catch (MissingRequiredPropertyException exception) {
+         // as expected
+      }
+
+      try {
+         PropertyReaderUtils.getRequiredProperty(r0, "a");
+         fail("Expected MissingRequiredPropertyException.");
+      } catch (MissingRequiredPropertyException exception) {
+         // as expected
+      }
+
+      try {
+         PropertyReaderUtils.getRequiredProperty(r0, "b");
+         fail("Expected MissingRequiredPropertyException.");
+      } catch (MissingRequiredPropertyException exception) {
+         // as expected
+      }
+
+      assertEquals("value", PropertyReaderUtils.getRequiredProperty(r0, "c"));
+   }
+
+   public void testPropertyReaderUtils_createPropertyReader()
+   throws Exception {
+
+      try {
+         PropertyReaderUtils.createPropertyReader(null);
+         fail("Expected IllegalArgumentException.");
+      } catch (IllegalArgumentException exception) {
+         // as expected
+      }
+
+      InputStream in0 = new ByteArrayInputStream("".getBytes("US-ASCII"));
+      InputStream in1 = new ByteArrayInputStream("a=\nb= \nc=1\nd=1\ne=2\nf=3 \n\n\t\n".getBytes("US-ASCII"));
+
+      PropertyReader r = PropertyReaderUtils.createPropertyReader(in0);
+      assertEquals(0, r.size());
+
+      r = PropertyReaderUtils.createPropertyReader(in1);
+      assertEquals(4, r.size());
+
+      assertEquals(null, r.get("a"));
+      assertEquals(null, r.get("b"));
+      assertEquals("1",  r.get("c"));
+      assertEquals("1",  r.get("d"));
+      assertEquals("2",  r.get("e"));
+      assertEquals("3 ", r.get("f"));
    }
 }
