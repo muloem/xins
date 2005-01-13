b08b87c8c02fa397fc486817900da031672d8662
znerd <>
2005-01-13 10:29:23 +0000

    Improved tests.

--- src/tests/org/xins/tests/common/service/DescriptorBuilderTests.java
+++ src/tests/org/xins/tests/common/service/DescriptorBuilderTests.java
@@ -7,12 +7,16 @@
 package org.xins.tests.common.service;
 
 import java.util.Properties;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import org.xins.common.collections.InvalidPropertyValueException;
 import org.xins.common.collections.MissingRequiredPropertyException;
 import org.xins.common.collections.PropertyReader;
 import org.xins.common.collections.PropertiesPropertyReader;
+
 import org.xins.common.service.Descriptor;
 import org.xins.common.service.DescriptorBuilder;
 import org.xins.common.service.GroupDescriptor;
@@ -85,8 +89,9 @@ public class DescriptorBuilderTests extends TestCase {
       _properties.clear();
    }
 
-   public void testBuild() throws Throwable {
+   public void testDescriptorBuilder() throws Throwable {
 
+      // Pass null to constructor
       try {
          DescriptorBuilder.build(null, null);
          fail("Expected build(null, null) to throw an IllegalArgumentException.");
@@ -111,12 +116,58 @@ public class DescriptorBuilderTests extends TestCase {
       String url = "http://somehost.somecompany.com:3003/something/else";
       long timeOut = 1500;
       String base = "server";
-      _properties.setProperty(base, DescriptorBuilder.TARGET_DESCRIPTOR_TYPE + DescriptorBuilder.DELIMITER + url + DescriptorBuilder.DELIMITER + String.valueOf(timeOut));
+      String prop = DescriptorBuilder.TARGET_DESCRIPTOR_TYPE
+                  + DescriptorBuilder.DELIMITER
+                  + url
+                  + DescriptorBuilder.DELIMITER
+                  + String.valueOf(timeOut);
+      _properties.setProperty(base, prop);
       Descriptor d = DescriptorBuilder.build(_propertyReader, base);
       assertNotNull(d);
       assertEquals(TargetDescriptor.class, d.getClass());
       TargetDescriptor target = (TargetDescriptor) d;
       assertEquals(url,     target.getURL());
       assertEquals(timeOut, target.getTotalTimeOut());
+
+      // GroupDescriptor with no members
+      String gprop = DescriptorBuilder.GROUP_DESCRIPTOR_TYPE
+                   + DescriptorBuilder.DELIMITER
+                   + GroupDescriptor.RANDOM_TYPE;
+      _properties.setProperty(base, gprop);
+      try {
+         DescriptorBuilder.build(_propertyReader, base);
+         fail("Expected InvalidPropertyValueException.");
+      } catch (InvalidPropertyValueException exception) {
+         // as expected
+      }
+
+      // GroupDescriptor with no members, with delimiter at end
+      gprop += DescriptorBuilder.DELIMITER;
+      _properties.setProperty(base, gprop);
+      try {
+         DescriptorBuilder.build(_propertyReader, base);
+         fail("Expected InvalidPropertyValueException.");
+      } catch (InvalidPropertyValueException exception) {
+         // as expected
+      }
+
+      // GroupDescriptor with one non-existing member
+      gprop += "t1";
+      _properties.setProperty(base, gprop);
+      try {
+         DescriptorBuilder.build(_propertyReader, base);
+         fail("Expected MissingRequiredPropertyException.");
+      } catch (MissingRequiredPropertyException exception) {
+         // as expected
+      }
+
+      // GroupDescriptor with one existing member
+      _properties.setProperty(base + ".t1", prop);
+      try {
+         DescriptorBuilder.build(_propertyReader, base);
+         fail("Expected InvalidPropertyValueException.");
+      } catch (InvalidPropertyValueException exception) {
+         // as expected
+      }
    }
 }
