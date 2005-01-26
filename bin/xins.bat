db204fb5a5d1522457350365fafb9a0744832125
znerd <>
2005-01-26 16:03:56 +0000

    Cleaned up shell script and batch file a bit.

--- bin/xins.bat
+++ bin/xins.bat
@@ -14,22 +14,25 @@
 @ECHO OFF
 
 :: Make sure XINS_HOME is set
-IF NOT ""%XINS_HOME%"" == """" GOTO show_version1
-ECHO FATAL: XINS_HOME not set
+IF NOT ""%XINS_HOME%"" == """" GOTO test_version
+ECHO xins.bat: FATAL: XINS_HOME not set
 GOTO end
 
-:show_version1
-IF NOT "%1" == "-version" GOTO show_version2
+:: Recognize version option
+:test_version
+IF "%1" == "-version" GOTO show_version
+IF "%1" == "version"  GOTO show_version
+GOTO start_build
+:show_version
 CALL ant -q -f %XINS_HOME%\build.xml version
 GOTO end
 
-:show_version2
-IF NOT "%1" == "version" GOTO start_build
-CALL ant -q -f %XINS_HOME%\build.xml version
-GOTO end
-
+:: Generate the Ant build file 
 :start_build
 CALL ant -q -lib %XINS_HOME%\lib -f %XINS_HOME%\src\ant\make-build.xml -Dxins_home=%XINS_HOME%
+IF ERRORLEVEL 1 GOTO end
 
-IF NOT ERRORLEVEL 1 CALL ant -lib %XINS_HOME%\lib -f build\build.xml %1 %2 %3 %4 %5 %6 %7 %8 %9
+:: Execute the Ant build file 
+CALL ant -lib %XINS_HOME%\lib -f build\build.xml %1 %2 %3 %4 %5 %6 %7 %8 %9
+
 :end
