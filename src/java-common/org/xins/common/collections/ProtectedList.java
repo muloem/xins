613d6eb2a3e38cb321b52b99a47dc5fc59516d30
znerd <>
2005-04-18 09:25:23 +0000

    Implemented RFE #1145386: ProtectedList class should throw IncorrectSecretKeyException.

--- src/java-common/org/xins/common/collections/ProtectedList.java
+++ src/java-common/org/xins/common/collections/ProtectedList.java
@@ -9,13 +9,24 @@ package org.xins.common.collections;
 import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 import org.xins.common.MandatoryArgumentChecker;
 
 /**
- * Implementation of a list that can only be modified using the secret key
- * passed to the constructor.
+ * Modifiable <code>List</code> implementaton that can be protected from
+ * unauthorized changes.
+ *
+ * <p>A secret key must be passed when constructing a
+ * <code>ProtectedPropertyReader</code> instance. All modification methods on
+ * this object then require this same secret key to be passed, otherwise they
+ * fail with an {@link IncorrectSecretKeyException}.
+ * 
+ * <p>Note that the secret key equality is always checked before the other
+ * preconditions. This means that if the secret key is incorrect, then the
+ * other preconditions will not even be checked. For example, if
+ * {@link #remove(Object,int) remove}(null, -1)</code> is called, then an
+ * {@link IncorrectSecretKeyException} is thrown for the mismatching secret
+ * key, and not an {@link IndexOutOfBoundsException}, for the negative index.
  *
  * @version $Revision$ $Date$
  * @author Anthony Goubard (<a href="mailto:anthony.goubard@nl.wanadoo.com">anthony.goubard@nl.wanadoo.com</a>)
@@ -59,6 +70,7 @@ public final class ProtectedList extends AbstractList implements Cloneable {
       // Check preconditions
       MandatoryArgumentChecker.check("secretKey", secretKey);
 
+      // Store secret key and construct internal list
       _secretKey = secretKey;
       _list      = new ArrayList(initialCapacity);
    }
@@ -80,6 +92,7 @@ public final class ProtectedList extends AbstractList implements Cloneable {
       // Check preconditions
       MandatoryArgumentChecker.check("secretKey", secretKey);
 
+      // Store secret key and construct internal list
       _secretKey = secretKey;
       _list      = new ArrayList();
    }
@@ -109,6 +122,7 @@ public final class ProtectedList extends AbstractList implements Cloneable {
       // Check preconditions
       MandatoryArgumentChecker.check("secretKey", secretKey, "c", c);
 
+      // Store secret key and construct internal list
       _secretKey = secretKey;
       _list      = new ArrayList(c);
    }
@@ -133,10 +147,52 @@ public final class ProtectedList extends AbstractList implements Cloneable {
    // Methods
    //-------------------------------------------------------------------------
 
+   /**
+    * Verifies that the specified object matches the secret key. If not, an
+    * exception is thrown.
+    *
+    * @param secretKey
+    *    the secret key, must be identity-equal to the secret key passed to
+    *    the constructor, cannot be <code>null</code>.
+    *
+    * @throws IncorrectSecretKeyException
+    *    if <code>secretKey</code> does not match the secret key passed to the
+    *    constructor.
+    */
+   private void checkSecretKey(Object secretKey)
+   throws IncorrectSecretKeyException {
+      if (secretKey != _secretKey) {
+         throw new IncorrectSecretKeyException();
+      }
+   }
+
+   /**
+    * Returns the element at the specified position in this list.
+    *
+    * @param index
+    *    the index of the element to return, must be &gt;= 0 and &lt;
+    *    {@link #size()}.
+    *
+    * @return
+    *    the element at the specified position in this list, can be
+    *    <code>null</code>.
+    *
+    * @throws IndexOutOfBoundsException
+    *    if <code>index &lt; 0
+    *          || index &gt;= {@link #size()}</code>.
+    */
    public Object get(int index) {
       return _list.get(index);
    }
 
+   /**
+    * Returns the number of elements in this list. If this list contains more
+    * than {@link Integer#MAX_VALUE} elements, then {@link Integer#MAX_VALUE}
+    * is returned.
+    *
+    * @return
+    *    the size of this list, maximized to {@link Integer#MAX_VALUE}.
+    */
    public int size() {
       return _list.size();
    }
@@ -144,29 +200,28 @@ public final class ProtectedList extends AbstractList implements Cloneable {
    /**
     * Adds the specified element to the list.
     *
-    * <p>The secret key must be passed. If it is incorrect, then an
-    * {@link IllegalArgumentException} is thrown. Note that an identity check
-    * is done, <em>not</em> an equality check. So
-    * {@link Object#equals(Object)} is not used, but the <code>==</code>
-    * operator is.
+    * <p>The correct secret key must be passed. If it is incorrect, then an
+    * {@link IncorrectSecretKeyException} is thrown. Note that an identity
+    * check is done, <em>not</em> an equality check. So
+    * the {@link Object#equals(Object)} method is not used, but the
+    * <code>==</code> operator is.
     *
     * @param secretKey
-    *    the secret key, must be the same as the key specified with the
-    *    constructor, cannot be <code>null</code>.
+    *    the secret key, must be identity-equal to the secret key passed to
+    *    the constructor, cannot be <code>null</code>.
     *
     * @param element
     *    the element to add to the list, can be <code>null</code>.
     *
-    * @throws IllegalArgumentException
-    *    if the secretKey is incorrect.
+    * @throws IncorrectSecretKeyException
+    *    if <code>secretKey</code> does not match the secret key passed to the
+    *    constructor.
     */
    public void add(Object secretKey, Object element)
-   throws IllegalArgumentException {
+   throws IncorrectSecretKeyException {
 
       // Check preconditions
-      if (secretKey != _secretKey) {
-         throw new IllegalArgumentException("Invalid key.");
-      }
+      checkSecretKey(secretKey);
 
       // Store the value
       _list.add(element);
@@ -175,37 +230,42 @@ public final class ProtectedList extends AbstractList implements Cloneable {
    /**
     * Removes the specified element.
     *
-    * <p>The key must be passed. If it is incorrect, then an
-    * {@link IllegalArgumentException} is thrown. Note that an identity check
-    * is done, <em>not</em> an equality check. So
-    * {@link Object#equals(Object)} is not used, but the <code>==</code>
-    * operator is.
+    * <p>The correct secret key must be passed. If it is incorrect, then an
+    * {@link IncorrectSecretKeyException} is thrown. Note that an identity
+    * check is done, <em>not</em> an equality check. So
+    * the {@link Object#equals(Object)} method is not used, but the
+    * <code>==</code> operator is.
     *
     * @param secretKey
-    *    the secret key, must be the same as the key specified with the
-    *    constructor, cannot be <code>null</code>.
+    *    the secret key, must be identity-equal to the secret key passed to
+    *    the constructor, cannot be <code>null</code>.
     *
     * @param index
-    *    the position of the element to remove.
+    *    the position of the element to remove, must be &gt;= 0 and &lt;
+    *    {@link #size()}.
     *
-    * @throws IllegalArgumentException
-    *    if the key is incorrect.
+    * @throws IncorrectSecretKeyException
+    *    if <code>secretKey</code> does not match the secret key passed to the
+    *    constructor.
+    *
+    * @throws IndexOutOfBoundsException
+    *    if <code>index &lt;  0
+    *          || index &gt;= {@link #size()}</code>.
     */
    public void remove(Object secretKey, int index)
-   throws IllegalArgumentException {
+   throws IncorrectSecretKeyException, IndexOutOfBoundsException {
 
       // Check preconditions
-      if (secretKey != _secretKey) {
-         throw new IllegalArgumentException("Invalid key.");
-      }
+      checkSecretKey(secretKey);
 
       // Remove the element
       _list.remove(index);
    }
 
    /**
-    * Clones this list. The cloned list will only be ediatable by using the
-    * same secret key.
+    * Clones this object. The returned object will be a new
+    * <code>ProtectedList</code> instance with the same secret key and the
+    * same elements.
     *
     * @return
     *    a new clone of this object, never <code>null</code>.
