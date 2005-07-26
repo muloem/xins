df9b692105c53fbdd322b625261f4f23a930aa85
tauseef442 <>
2005-07-26 14:16:44 +0000

    Implemented SF.net RFE #1190166: Make XINS_HOME environment variable optional.

--- bin/xins.bat
+++ bin/xins.bat
@@ -8,9 +8,16 @@
 @ECHO OFF
 
 :: Make sure XINS_HOME is set
+set __XINS_HOME=%XINS_HOME%
 IF NOT ""%XINS_HOME%"" == """" GOTO test_version
-ECHO xins.bat: FATAL: XINS_HOME not set
-GOTO end
+
+:: If XINS_HOME is not set, it should be the directory of the script.
+for %%x in (%0) do set __XINS_HOME=%%~dpsx
+for %%x in (%__XINS_HOME%) do set  __XINS_HOME=%%~dpsx
+set __XINS_HOME=%__XINS_HOME%..\
+
+ECHO XINS_HOME not set, Setting the scripts directory as XINS_HOME=%__XINS_HOME%
+GOTO test_version
 
 :: Recognize version option
 :test_version
@@ -18,12 +25,12 @@ IF "%1" == "-version" GOTO show_version
 IF "%1" == "version"  GOTO show_version
 GOTO start_build
 :show_version
-CALL ant -q -f %XINS_HOME%\build.xml version
+CALL ant -q -f %__XINS_HOME%\build.xml version
 GOTO end
 
 :: Generate the Ant build file
 :start_build
-CALL ant -f %XINS_HOME%\src\ant\make-build.xml -Dxins_home=%XINS_HOME%
+CALL ant -f %__XINS_HOME%\src\ant\make-build.xml -Dxins_home=%__XINS_HOME%
 IF ERRORLEVEL 1 GOTO end
 
 :: Execute the Ant build file
