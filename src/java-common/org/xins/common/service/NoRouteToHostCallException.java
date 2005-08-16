6d628cd4cad0e1b03879d645c7beb138db7e0bac
znerd <>
2005-08-16 06:49:18 +0000

    Initial version.

---
+++ src/java-common/org/xins/common/service/NoRouteToHostCallException.java
@@ -0,0 +1,76 @@
+/*
+ * $Id$
+ *
+ * Copyright 2003-2005 Wanadoo Nederland B.V.
+ * See the COPYRIGHT file for redistribution and use restrictions.
+ */
+package org.xins.common.service;
+
+/**
+ * Exception that indicates that a connection to a service could not be
+ * established because no network route could be found to the host.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:ernst.dehaan@nl.wanadoo.com">ernst.dehaan@nl.wanadoo.com</a>)
+ *
+ * @since XINS 1.3.0
+ */
+public final class NoRouteToHostCallException
+extends ConnectionCallException {
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   /**
+    * Fully-qualified name of this class.
+    */
+   private static final String CLASSNAME =
+      NoRouteToHostCallException.class.getName();
+
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>NoRouteToHostCallException</code>.
+    *
+    * @param request
+    *    the original request, cannot be <code>null</code>.
+    *
+    * @param target
+    *    descriptor for the target that was attempted to be called, cannot be
+    *    <code>null</code>.
+    *
+    * @param duration
+    *    the call duration in milliseconds, must be &gt;= 0.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>request     == null
+    *          || target      == null
+    *          || duration  &lt; 0</code>.
+    */
+   public NoRouteToHostCallException(CallRequest      request,
+                                     TargetDescriptor target,
+                                     long             duration)
+   throws IllegalArgumentException {
+
+      // Trace and then call constructor of superclass
+      super("No route to host",
+            request, target, duration, null, null);
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+}
