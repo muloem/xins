b017c23b7ca3dc54430914d17bb8baa186d44cc8
znerd <>
2005-09-14 15:11:21 +0000

    * XINS/Java Common Library:   - Improved the HostnameTask (for Apache Ant) so it now attempts calling the     'hostname' command on UNIX/Linux systems, if no information can be     retrieved from the JDK.

--- src/java-common/org/xins/common/ant/HostnameTask.java
+++ src/java-common/org/xins/common/ant/HostnameTask.java
@@ -6,9 +6,12 @@
  */
 package org.xins.common.ant;
 
+import java.io.InputStream;
+
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
+
 import org.xins.common.net.IPAddressUtils;
 
 /**
@@ -34,6 +37,77 @@ public class HostnameTask extends Task {
    //-------------------------------------------------------------------------
    // Class functions
    //-------------------------------------------------------------------------
+
+   /**
+    * Tries running the POSIX <code>hostname</code> command to determine the
+    * name of the local host. If this does not succeed, then <code>null</code>
+    * is returned instead of the hostname.
+    *
+    * @return
+    *    the name of the local host, or <code>null</code>.
+    */
+   private static String runHostnameCommand() {
+
+      String hostname;
+      try {
+         Process process = Runtime.getRuntime().exec("hostname");
+         process.waitFor();
+
+         int exitValue = process.exitValue();
+         if (exitValue != 0) {
+            // TODO: Log
+            return null;
+         }
+
+         // Get the stdout output from the process
+         InputStream in = process.getInputStream();
+
+         // Configure max expected hostname length
+         final int MAX = 500;
+
+         // Read the whole output
+         byte[] bytes = new byte[MAX];
+         int read = in.read(bytes);
+         if (read < 0) {
+            return null;
+         }
+
+         // TODO: Check all characters in the hostname
+
+         final String ENCODING = "US-ASCII";
+         hostname = new String(bytes, 0, read, ENCODING);
+
+         for (int i = 0; i < read; i++) {
+            char ch = hostname.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+               // OK: fall through
+            } else if (ch > 'A' && ch <= 'Z') {
+               // OK: fall through
+            } else if (ch > '0' && ch <= '9') {
+               // OK: fall through
+            } else if (ch == '-' || ch == '_' || ch == '.') {
+               // OK: fall through
+            } else if (ch == '\n' || ch == '\r') {
+               hostname = hostname.substring(0, i);
+               i = read;
+            }
+         }
+
+      } catch (Exception exception) {
+         // TODO: Log
+         hostname = null;
+      }
+
+      if (hostname != null) {
+         hostname = hostname.trim();
+         if (hostname.length() < 1) {
+            hostname = null;
+         }
+      }
+
+      return hostname;
+   }
+
 
    //-------------------------------------------------------------------------
    // Constructors
@@ -85,17 +159,40 @@ public class HostnameTask extends Task {
     */
    public void execute() throws BuildException {
 
+      // Do not override the property value
       if (getProject().getUserProperty(_propertyName) != null) {
          log("Override ignored for property \"" + _propertyName + "\".", Project.MSG_VERBOSE);
          return;
       }
 
+      // First try using the IPAddressUtils class from the JDK
       String hostname = IPAddressUtils.getLocalHost();
 
-      if (hostname == null || "".equals(hostname.trim()) || "localhost".equals(hostname.trim())) {
-         log("Determining hostname of localhost failed. Setting property to \"" + _propertyName + "\".", Project.MSG_WARN);
+      // Normalize the host name and filter out fakes (empty or "localhost")
+      if (hostname != null) {
+         hostname = hostname.trim();
+         if ("".equals(hostname) || "localhost".equals(hostname)) {
+            hostname = null;
+         }
       }
 
+      // No hostname yet? Try running the 'hostname' command on POSIX systems
+      if (hostname == null) {
+         hostname = runHostnameCommand();
+      }
+
+      // Still no hostname, fallback to a default and then log a warning
+      if (hostname == null) {
+         hostname = "localhost";
+
+         String message = "Determining hostname of localhost failed. "
+                        + "Setting property to \""
+                        + hostname
+                        + "\".";
+         log(message, Project.MSG_WARN);
+      }
+
+      // Actually set the property
       getProject().setUserProperty(_propertyName, hostname);
    }
 }
