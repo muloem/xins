aa9579ac5238fe43d28f915e29b161943b3fcbc9
znerd <>
2005-09-29 12:19:57 +0000

    Improved toDateString(long,boolean,String) slightly.

--- src/java-common/org/xins/common/text/DateConverter.java
+++ src/java-common/org/xins/common/text/DateConverter.java
@@ -31,7 +31,7 @@ public class DateConverter extends Object {
    //-------------------------------------------------------------------------
 
    /**
-    * Two-length string representations of the digits 0 to 59.
+    * Two-length string representations of the digits 0 to 99.
     */
    private static final String[] VALUES = new String[] {
       "00", "01", "02", "03", "04", "05", "06", "07", "08", "09",
@@ -39,7 +39,12 @@ public class DateConverter extends Object {
       "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
       "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
       "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
-      "50", "51", "52", "53", "54", "55", "56", "57", "58", "59" };
+      "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
+      "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+      "70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
+      "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
+      "90", "91", "92", "93", "94", "95", "96", "97", "98", "99",
+   };
 
 
    //-------------------------------------------------------------------------
@@ -82,7 +87,7 @@ public class DateConverter extends Object {
 
       FastStringBuffer buffer = new FastStringBuffer(23);
 
-      Calendar calendar = Calendar.getInstance(timeZone);
+      GregorianCalendar calendar = new GregorianCalendar(timeZone);
 
       // XXX: This works with Java 1.4+ only
       // calendar.setTimeInMillis(n);
@@ -99,7 +104,7 @@ public class DateConverter extends Object {
       int ms    = calendar.get(Calendar.MILLISECOND);
 
       // Append year followed by a dot, length is now 5
-      buffer.append(String.valueOf(year));
+      buffer.append(year);
       buffer.append('.');
 
       // Append month followed by a dot, length is now 8
@@ -134,26 +139,37 @@ public class DateConverter extends Object {
    }
 
    /**
-    * Convert the current time to a <code>String</code>.
+    * Formats a timestamp as a <code>String</code>.
     *
-    * @param century
+    * @param millis
+    *    the timestamp, as a number of milliseconds since the Epoch.
+    *
+    * @param withCentury
     *    <code>true</code> if the century should be in the result, 
     *    <code>false</code> otherwise.
     *
     * @param separator
-    *    the separator between the date and the hours, or <code>null</code>
-    *    if no separator should be set.
+    *    the separator between the date and the hours, never <code>null</code>
+    *    (can be an empty string though).
     *
     * @return
     *    the converted character string, cannot be <code>null</code>.
     *
     * @since XINS 1.3.0
     */
-   public static String toDateString(boolean century, String separator) {
-      Calendar calendar = GregorianCalendar.getInstance();
+   public static String toDateString(long    millis,
+                                     boolean withCentury,
+                                     String  separator) {
+
+      // Check preconditions
+      MandatoryArgumentChecker.check("separator", separator);
+
+      // Convert the millis to a GregorianCalendar instance
+      GregorianCalendar calendar = new GregorianCalendar();
+      Date date = new Date(millis);
+      calendar.setDate(date);
       
-      FastStringBuffer buffer = new FastStringBuffer(23);
-      
+      // Get all individual fields from the calendar
       int year  = calendar.get(Calendar.YEAR);
       int month = calendar.get(Calendar.MONTH);
       int day   = calendar.get(Calendar.DAY_OF_MONTH);
@@ -162,20 +178,30 @@ public class DateConverter extends Object {
       int sec   = calendar.get(Calendar.SECOND);
       int ms    = calendar.get(Calendar.MILLISECOND);
       
-      if (century) {
-         buffer.append(String.valueOf(year));
-      } else {
-         buffer.append(String.valueOf(year).substring(2));
+      // Add century and year or both
+      FastStringBuffer buffer = new FastStringBuffer(23);
+      int century  = year / 100;
+      int yearOnly = year % 100;
+      if (withCentury) {
+         buffer.append(VALUES[century]);
       }
-      buffer.append(VALUES[month + 1]); // Month is 0-based
+      buffer.append(VALUES[yearOnly]);
+
+      // Add month (which is 0-based, so we need to add 1)
+      buffer.append(VALUES[month + 1]);
+
+      // Add day
       buffer.append(VALUES[day]);
-      if (separator != null) {
-         buffer.append(separator);
-      }
+
+      // Add separator between date and time
+      buffer.append(separator);
+
+      // Add hours, minutes and seconds
       buffer.append(VALUES[hour]);
       buffer.append(VALUES[min]);
       buffer.append(VALUES[sec]);
          
+      // Add milliseconds
       if (ms < 10) {
          buffer.append("00");
       } else if (ms < 100) {
