7a9a52e11a7d7c4a2877849102403e7d938ba6ce
znerd <>
2005-10-03 10:56:25 +0000

    Initial version.

---
+++ src/tests/org/xins/perftests/MandatoryArgumentCheckerTests.java
@@ -0,0 +1,76 @@
+/*
+ * $Id$
+ *
+ * Copyright 2003-2005 Wanadoo Nederland B.V.
+ * See the COPYRIGHT file for redistribution and use restrictions.
+ */
+package org.xins.perftests;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.xins.common.MandatoryArgumentChecker;
+
+/**
+ * Performance tests for class <code>MandatoryArgumentChecker</code>.
+ *
+ * @version $Revision$ $Date$
+ * @author Ernst de Haan (<a href="mailto:ernst.dehaan@nl.wanadoo.com">ernst.dehaan@nl.wanadoo.com</a>)
+ */
+public class MandatoryArgumentCheckerTests extends TestCase {
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   /**
+    * Returns a test suite with all test cases defined by this class.
+    *
+    * @return
+    *    the test suite, never <code>null</code>.
+    */
+   public static Test suite() {
+      return new TestSuite(MandatoryArgumentCheckerTests.class);
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   private static final int ROUNDS = 100000000;
+
+
+   //-------------------------------------------------------------------------
+   // Constructor
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>MandatoryArgumentCheckerTests</code> test suite
+    * with the specified name. The name will be passed to the superconstructor.
+    *
+    * @param name
+    *    the name for this test suite.
+    */
+   public MandatoryArgumentCheckerTests(String name) {
+      super(name);
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Fields
+   //-------------------------------------------------------------------------
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+
+   public void testMandatoryArgumentChecker() throws Exception {
+
+      for (int i = 0; i < ROUNDS; i++) {
+         MandatoryArgumentChecker.check("something", "not null");
+         MandatoryArgumentChecker.check("a", "", "b", "c");
+      }
+   }
+}
