2f542c63f68565109afb5449a671fcd0a65cbe60
znerd <>
2005-10-04 08:37:24 +0000

    Made constructor public and added a method format(long,char[],int) for very fast date formatting.

--- src/java-common/org/xins/common/text/DateConverter.java
+++ src/java-common/org/xins/common/text/DateConverter.java
@@ -11,6 +11,8 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
+import java.text.SimpleDateFormat;
+
 import org.xins.common.MandatoryArgumentChecker;
 
 /**
@@ -69,8 +71,6 @@ public class DateConverter extends Object {
    //-------------------------------------------------------------------------
 
    static {
-      // XXX: Allow test coverage analysis tools to report 100% coverage
-      new DateConverter();
 
       // Fill the VALUES array
       VALUES = new char[100][];
@@ -318,15 +318,38 @@ public class DateConverter extends Object {
       buffer.append(millis);
    }
 
+
    //-------------------------------------------------------------------------
    // Constructors
    //-------------------------------------------------------------------------
 
    /**
-    * Creates a new <code>DateConverter</code> object.
+    * Creates a new <code>DateConverter</code>.
+    *
+    * @param withCentury
+    *    <code>true</code> if the century should be in the result, 
+    *    <code>false</code> otherwise.
+    *
+    * @since XINS 1.3.0
     */
-   private DateConverter() {
-      // empty
+   public DateConverter(boolean withCentury) {
+
+      // Store the flag
+      _withCentury = withCentury;
+
+      // Determine the length of the formatted date strings
+      _length           = withCentury ? 18 : 16;
+      _cachedDateBuffer = new char[_length];
+
+      // Construct a formatter for slow formatting
+      String format = "yyMMdd-HHmmssSSS";
+      if (withCentury) {
+         format = "yy" + format;
+      }
+      _formatter = new SimpleDateFormat(format);
+
+      // Pre-cache the current date
+      recompute(System.currentTimeMillis());
    }
 
 
@@ -334,8 +357,159 @@ public class DateConverter extends Object {
    // Fields
    //-------------------------------------------------------------------------
 
+   /**
+    * Date formatter that is used as a slow but accurate method for formatting
+    * the date. Never <code>null</code>.
+    */
+   private final SimpleDateFormat _formatter;
+
+   /**
+    * Flag that indicates whether the century should be in the formatted date.
+    * Is <code>true</code> if the century should be in there, or
+    * <code>false</code> if not.
+    */
+   private final boolean _withCentury;
+
+   /**
+    * Length of the produced timestamp string. If the century is printed, it's
+    * 18, otherwise it's 16.
+    */
+   private final int _length;
+
+   /**
+    * Cached date, as a number of milliseconds since the Epoch.
+    */
+   private long _cachedDate;
+
+   /**
+    * Cached date, as a number of minutes since the Epoch.
+    */
+   private long _cachedMinutes;
+
+   /**
+    * Part of the cached date, the number of seconds in the current minute.
+    */
+   private long _cachedJustSeconds;
+
+   /**
+    * Character buffer containing the formatted version of the cached date.
+    * See {@link #_cachedDate}. The length is always equal to
+    * {@link #_length}.
+    */
+   private char[] _cachedDateBuffer;
+
+
    //-------------------------------------------------------------------------
    // Methods
    //-------------------------------------------------------------------------
-   
+
+   /**
+    * Recomputes the cached formatted date.
+    *
+    * @param date
+    *    the timestamp to reinitialize the cache with, as a number of
+    *    milliseconds since the Epoch.
+    */
+   private void recompute(long date) {
+
+      // Store the cached date
+      _cachedDate        = date;
+      _cachedMinutes     = (date / 60000L);
+      long seconds       = (date /  1000L);
+      _cachedJustSeconds = (int) (seconds %    60L);
+
+      // Format the date
+      String s = _formatter.format(new Date(_cachedDate));
+      s.getChars(0, _length, _cachedDateBuffer, 0);
+   }
+
+   /**
+    * Formats the specified timestamp as a <code>String</code>. Depending on
+    * the setting of the <em>withCentury</em> property (passed to the
+    * constructor), the format is as follows. If <em>withCentury</em> is set,
+    * then the format is:
+    *
+    * <blockquote><em>CCYYMMDD-hhmmssSSS</em> (length is 18)</blockquote>
+    *
+    * Otherwise, if <em>withCentury</em> is not set, then the format is
+    * without the century:
+    * 
+    * <blockquote><em>YYMMDD-hhmmssSSS</em> (length is 16)</blockquote>
+    *
+    * <p>Note: This method is <em>not</em> thread-safe.
+    *
+    * @param date
+    *    the timestamp, in milliseconds since the Epoch (midnight at the start
+    *    of January 1, 1970, GMT); must be &gt;= <code>0L</code>.
+    *
+    * @param buffer
+    *    the character buffer to put the formatted timestamp in, cannot be
+    *    <code>null</code>.
+    *
+    * @param offset
+    *    the offset into the character buffer.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>date &lt; 0L</code>.
+    *
+    * @throws NullPointerException
+    *    if <code>buffer == null</code>.
+    *
+    * @throws IndexOutOfBoundsException
+    *    if <code>offset</code> is invalid (less than <code>0</code>) or
+    *    incorrect (not leaving enough room for the formatter date).
+    *
+    * @since XINS 1.3.0
+    */
+   public void format(long date, char[] buffer, int offset)
+   throws IllegalArgumentException,
+          NullPointerException,
+          IndexOutOfBoundsException {
+
+      // Check precondition
+      if (date < 0L) {
+         throw new IllegalArgumentException("date (" + date + ") < 0L");
+      }
+
+      // Cache the length of the generated string
+      int length = _length;
+
+      // Compute the delta with the cached date
+      long delta = date - _cachedDate;
+
+      // If we are in the same millisecond, then short-circuit
+      if (delta == 0) {
+         System.arraycopy(_cachedDateBuffer, 0, buffer, offset, length);
+         return;
+      }
+
+      // Determine the number of seconds and milliseconds
+      long minutes     =       (date    / 60000L);
+      long seconds     =       (date    /  1000L);
+      int  justSeconds = (int) (seconds %    60L);
+      int  justMillis  = (int) (date    %  1000L);
+
+      // We are in the same minute
+      if (minutes == _cachedMinutes) {
+
+         // First copy the whole cached formatted string
+         System.arraycopy(_cachedDateBuffer, 0, buffer, offset, length);
+
+         // If we are not in the same second, correct the seconds
+         if (justSeconds != _cachedJustSeconds) {
+            buffer[length - 5] = (char) (ZERO + (justSeconds / 10));
+            buffer[length - 4] = (char) (ZERO + (justSeconds % 10));
+         }
+
+         // Correct the milliseconds
+         buffer[length - 3] = (char) (ZERO + ( justMillis / 100)      );
+         buffer[length - 2] = (char) (ZERO + ((justMillis % 100) / 10));
+         buffer[length - 1] = (char) (ZERO + ( justMillis %  10)      );
+
+      // We are not in the same minute, so recompute
+      } else {
+         recompute(date);
+         System.arraycopy(_cachedDateBuffer, 0, buffer, offset, length);
+      }
+   }
 }
