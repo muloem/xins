7db1aa2afd1f23df6c79d3b8f3b1b61f572ac5c2
znerd <>
2005-10-06 12:45:31 +0000

    Preparation for more detail in the ProgrammingException message.

--- src/java-common/org/xins/common/ProgrammingException.java
+++ src/java-common/org/xins/common/ProgrammingException.java
@@ -23,6 +23,43 @@ extends RuntimeException {
    // Class fields
    //-------------------------------------------------------------------------
 
+   /**
+    * Creates the exception message for the specified details.
+    *
+    * @param detectingClass
+    *    the name of the class that detected the problem, or
+    *    <code>null</code> if unknown.
+    *
+    * @param detectingMethod
+    *    the name of the method within the <code>detectingClass</code> that
+    *    detected the problem, or <code>null</code> if unknown.
+    *
+    * @param subjectClass
+    *    the name of the class which exposes the programming error, or
+    *    <code>null</code> if unknown.
+    *
+    * @param subjectMethod
+    *    the name of the method (within the <code>subjectClass</code>) which
+    *    exposes the programming error, or <code>null</code> if unknown.
+    *
+    * @param detail
+    *    the detail message, can be <code>null</code>.
+    *
+    * @param cause
+    *    the cause exception, can be <code>null</code>.
+    */
+   private static String createMessage(String    detectingClass,
+                                       String    detectingMethod,
+                                       String    subjectClass,
+                                       String    subjectMethod,
+                                       String    detail,
+                                       Throwable cause) {
+
+      // TODO: Include all arguments in the exception message
+      return detail;
+   }
+
+
    //-------------------------------------------------------------------------
    // Class functions
    //-------------------------------------------------------------------------
@@ -64,10 +101,13 @@ extends RuntimeException {
                                String    subjectMethod,
                                String    detail,
                                Throwable cause) {
-      super(detail);
+      super(createMessage(detectingClass,detectingMethod,
+                          subjectClass,
+                          subjectMethod,
+                          detail,
+                          cause));
       ExceptionUtils.setCause(this, cause);
 
-      // TODO: Include all arguments in the exception message
       // TODO: Store arguments in fields
       // TODO: Add getters for arguments stored in fields
    }
