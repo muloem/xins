5f66de732ffbd0d471d8ff48736b0241001a2c09
znerd <>
2005-10-10 09:57:26 +0000

    - Added getters for detecting class/method, subject class/method and detail   in class ProgrammingException.

--- src/java-common/org/xins/common/ProgrammingException.java
+++ src/java-common/org/xins/common/ProgrammingException.java
@@ -134,12 +134,22 @@ extends RuntimeException {
                                String    subjectMethod,
                                String    detail,
                                Throwable cause) {
-      super(createMessage(detectingClass,detectingMethod,
-                          subjectClass,
-                          subjectMethod,
+
+      // Call superconstructor with a constructed message
+      super(createMessage(detectingClass, detectingMethod,
+                          subjectClass,   subjectMethod,
                           detail,
                           cause));
+
+      // Register the cause for this exception
       ExceptionUtils.setCause(this, cause);
+
+      // Store all the information in fields
+      _detectingClass  = detectingClass;
+      _detectingMethod = detectingMethod;
+      _subjectClass    = subjectClass;
+      _subjectMethod   = subjectMethod;
+      _detail          = detail;
    }
 
 
@@ -147,7 +157,101 @@ extends RuntimeException {
    // Fields
    //-------------------------------------------------------------------------
 
+   /**
+    * The name of the class that detected the problem. Is <code>null</code> if
+    * unknown.
+    */
+   private final String _detectingClass;
+
+   /**
+    * The name of the method where the problem was detected. Is
+    * <code>null</code> if unknown.
+    */
+   private final String _detectingMethod;
+
+   /**
+    * The name of the class which exposes the programming error. Is
+    * <code>null</code> if unknown.
+    */
+   private final String _subjectClass;
+
+   /**
+    * The name of the method which exposes the programming error, or
+    * <code>null</code> if unknown.
+    */
+   private final String _subjectMethod;
+
+   /**
+    * The detail message. Can be <code>null</code>.
+    */
+   private final String _detail;
+
+
    //-------------------------------------------------------------------------
    // Methods
    //-------------------------------------------------------------------------
+
+   /**
+    * Returns the name of the class that detected the problem.
+    *
+    * @return
+    *    the name of the class that detected the problem, or <code>null</code>
+    *    if unknown.
+    *
+    * @since XINS 1.3.0
+    */
+   public String getDetectingClass() {
+      return _detectingClass;
+   }
+
+   /**
+    * Returns the name of the method where the problem was detected.
+    *
+    * @return
+    *    the name of the method that detected the problem, or
+    *    <code>null</code> if unknown.
+    *
+    * @since XINS 1.3.0
+    */
+   public String getDetectingMethod() {
+      return _detectingMethod;
+   }
+
+   /**
+    * Returns the name of the class which exposes the programming error.
+    *
+    * @return
+    *    the name of the class that exposed the problem, or
+    *    <code>null</code> if unknown.
+    *
+    * @since XINS 1.3.0
+    */
+   public String getSubjectClass() {
+      return _subjectClass;
+   }
+
+   /**
+    * Returns the name of the method which exposes the programming error.
+    *
+    * @return
+    *    the name of the method that exposed the problem, or
+    *    <code>null</code> if unknown.
+    *
+    * @since XINS 1.3.0
+    */
+   public String getSubjectMethod() {
+      return _subjectMethod;
+   }
+
+   /**
+    * Returns the detail message.
+    *
+    * @return
+    *    the detail message, can be <code>null</code>.
+    *
+    * @since XINS 1.3.0
+    */
+   public String getDetail() {
+      return _detail;
+   }
 }
