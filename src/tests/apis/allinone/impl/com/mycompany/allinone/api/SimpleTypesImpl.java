253abd93019844bf8e62e7f620b6d125f4cf1daf
agoubard <>
2006-01-27 15:59:38 +0000

    Fixed broken perf tests.

--- src/tests/apis/allinone/impl/com/mycompany/allinone/api/SimpleTypesImpl.java
+++ src/tests/apis/allinone/impl/com/mycompany/allinone/api/SimpleTypesImpl.java
@@ -59,7 +59,11 @@ public class SimpleTypesImpl extends SimpleTypes  {
       result.setOutputLong(14L);
       result.setOutputFloat(3.5F);
       result.setOutputDouble(3.1415);
-      result.setOutputText("Hello ~!@#$%^&*()_+<>?[]\\;',./ \u20AC\u0630&");
+      if (request.getInputText().equals("perftests")) {
+        result.setOutputText("hello");
+      } else {
+        result.setOutputText("Hello ~!@#$%^&*()_+<>?[]\\;',./ \u20AC\u0630&");
+      }
       Date.Value outputDate = new Date.Value(2004,6,21);
       result.setOutputDate(outputDate);
 
