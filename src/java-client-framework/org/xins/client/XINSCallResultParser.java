28d8a602b112c80a6e825a78eb7026f6ba98eec9
agoubard <>
2006-03-21 15:30:47 +0000

    Avoid NullPointerException is the namespaceURI is null.

--- src/java-client-framework/org/xins/client/XINSCallResultParser.java
+++ src/java-client-framework/org/xins/client/XINSCallResultParser.java
@@ -546,7 +546,7 @@ extends Object {
          _state = ERROR;
 
          // Make sure namespaceURI is either null or non-empty
-         namespaceURI = "".equals(namespaceURI.trim())
+         namespaceURI = (namespaceURI != null && "".equals(namespaceURI.trim()))
                       ? null
                       : namespaceURI;
 
@@ -736,8 +736,6 @@ extends Object {
        */
       public void characters(char[] ch, int start, int length)
       throws IndexOutOfBoundsException, SAXException {
-
-         final String THIS_METHOD = "characters(char[],int,int)";
 
          // Temporarily enter ERROR state, on success this state is left
          State currentState = _state;
