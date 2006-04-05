084f38c208703bee7965a10680fab2ed0b5eedd2
agoubard <>
2006-04-05 09:30:14 +0000

    - Added test for the TextUtils#replace method.

---
+++ src/tests/org/xins/tests/common/text/TextUtilsTests.java
@@ -0,0 +1,86 @@
+/*
+ * $Id$
+ *
+ * Copyright 2003-2006 Wanadoo Nederland B.V.
+ * See the COPYRIGHT file for redistribution and use restrictions.
+ */
+package org.xins.tests.common.text;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Properties;
+import java.util.Random;
+import java.util.TimeZone;
+import java.text.SimpleDateFormat;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.xins.common.text.TextUtils;
+import org.xins.common.text.FastStringBuffer;
+
+/**
+ * Tests for class <code>TextUtils</code>.
+ *
+ * @version $Revision$ $Date$
+ * @author Anthony Goubard (<a href="mailto:anthony.goubard@nl.wanadoo.com">anthony.goubard@nl.wanadoo.com</a>)
+ */
+public class TextUtilsTests extends TestCase {
+
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   /**
+    * Returns a test suite with all test cases defined by this class.
+    *
+    * @return
+    *    the test suite, never <code>null</code>.
+    */
+   public static Test suite() {
+      return new TestSuite(TextUtilsTests.class);
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Constructor
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>TextUtilsTests</code> test suite with
+    * the specified name. The name will be passed to the superconstructor.
+    *
+    * @param name
+    *    the name for this test suite.
+    */
+   public TextUtilsTests(String name) {
+      super(name);
+   }
+
+
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+
+   public void testReplace() throws Exception {
+      String text = "hello ${world} ${name}";
+      Properties replaceMap = new Properties();
+      
+      String replace1 = TextUtils.replace(text, replaceMap, "${", "}");
+      assertEquals("Nothing should have been replaced", text, replace1);
+      
+      replaceMap.put("world", "world");
+      String replace2 = TextUtils.replace(text, replaceMap, "${", "}");
+      assertEquals("Incorrect text replaced.", "hello world ${name}", replace2);
+      
+      replaceMap.put("world", "");
+      String replace3 = TextUtils.replace(text, replaceMap, "${", "}");
+      assertEquals("Incorrect text replaced.", "hello  ${name}", replace3);
+      
+      replaceMap.put("world", "Mr");
+      replaceMap.put("name", "Anthony");
+      String replace4 = TextUtils.replace(text, replaceMap, "${", "}");
+      assertEquals("Incorrect text replaced.", "hello Mr Anthony", replace4);
+   }
+}
