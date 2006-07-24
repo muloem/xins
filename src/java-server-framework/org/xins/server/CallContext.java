0a8b68c4899ed1c3408f7e51fa1d6b99ce414e96
znerd <>
2006-07-24 08:06:22 +0000

    Added @see System#currentTimeMillis() for the getStart() method.

--- src/java-server-framework/org/xins/server/CallContext.java
+++ src/java-server-framework/org/xins/server/CallContext.java
@@ -116,6 +116,8 @@ extends Object {
     * @return
     *    the timestamp indicating when the call was started, as a number of
     *    milliseconds since midnight January 1, 1970 UTC.
+    *
+    * @see System#currentTimeMillis()
     */
    public long getStart() {
       return _start;
