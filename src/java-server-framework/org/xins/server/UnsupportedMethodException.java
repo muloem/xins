2c51f57ada505dfcd3876f2bf13425a0c6de4db2
znerd <>
2006-08-25 13:36:16 +0000

    Initial version.

---
+++ src/java-server-framework/org/xins/server/UnsupportedMethodException.java
@@ -0,0 +1,34 @@
+/*
+ * $Id$
+ *
+ * Copyright 2003-2006 Wanadoo Nederland B.V.
+ * See the COPYRIGHT file for redistribution and use restrictions.
+ */
+package org.xins.server;
+
+import org.xins.logdoc.ExceptionUtils;
+
+/**
+ * Exception that indicates that an incoming request is considered invalid
+ * because the HTTP method is unsupported for function invocations.
+ *
+ * @version $Revision$ $Date$
+ * @author <a href="mailto:ernst.dehaan@orange-ft.com">Ernst de Haan</a>
+ */
+public final class UnsupportedMethodException
+extends InvalidRequestException {
+
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>UnsupportedMethodException</code>.
+    *
+    * @param detail
+    *    description of the issue, can be <code>null</code>.
+    */
+   public UnsupportedMethodException(String detail) {
+      super(detail);
+   }
+}
