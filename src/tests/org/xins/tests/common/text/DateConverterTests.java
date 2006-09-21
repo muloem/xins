923e6a5bf7e542013baf99d7740456e7f850d0ab
agoubard <>
2006-09-21 14:12:26 +0000

    Added new test for new method. Removed test case as it's not a JDK bug the hh in the SimpleDateFormat should have been an HH.

--- src/tests/org/xins/tests/common/text/DateConverterTests.java
+++ src/tests/org/xins/tests/common/text/DateConverterTests.java
@@ -158,19 +158,14 @@ public class DateConverterTests extends TestCase {
       }
    }
 
-   /**
-    * Tests a possible JDK bug in {@link java.util.Date#after}.
-    */
-   /* This is not a XINS bug but a JDK bug.
-   public void testIncorrectDateAfter() {
-      SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd hh:mm:ss.SSS");
-      try {
-         Date beforeNoon = formatter.parse("2005.10.20 11:59:37.932");
-         Date afterNoon = formatter.parse("2005.10.20 12:00:02.674");
-         assertTrue("The morning is not before the afternoon.", beforeNoon.before(afterNoon));
-         assertTrue("The afternoon is not after the morning.", afterNoon.after(beforeNoon));
-      } catch (ParseException parseException) {
-         fail("Incorrect date format for startup time.");
-      }
-   }*/
+   public void testToDateString3() throws Exception {
+      String epoch = DateConverter.toDateString(0L);
+      assertTrue("Incorrect epoch time", epoch.startsWith("1970.01.01 "));
+      
+      SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss.SSS");
+      long now = System.currentTimeMillis();
+      String expected = formatter.format(new Date(now));
+      String actual = DateConverter.toDateString(now);
+      assertEquals("Incorrect date.", expected, actual);
+   }
 }
