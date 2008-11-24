2e84168dee34b2218c6d61c2b28cc9c5a0c4a255
agoubard <>
2008-11-24 21:39:37 +0000

    - Fixed bug #1861811: Added possibility to specify an authority in a URL.

--- src/java-common/org/xins/common/service/TargetDescriptor.java
+++ src/java-common/org/xins/common/service/TargetDescriptor.java
@@ -56,7 +56,7 @@ public final class TargetDescriptor extends Descriptor {
    /**
     * The pattern for a URL, as a character string.
     */
-   private static final String PATTERN_STRING = "[a-z][a-z\\d]*(:[a-z\\d]+)?:\\/\\/[a-z\\d-]*(\\.[a-z\\d-]*)*(:[1-9][\\d]*)?(\\/([a-z\\d%_~.-]*))*";
+   private static final String PATTERN_STRING = "[a-z][a-z\\d]*(:[a-z\\d]+)?:\\/\\/([\\w%~-]+:[\\w%~-]+@)?[a-z\\d-]*(\\.[a-z\\d-]*)*(:[1-9][\\d]*)?(\\/([a-z\\d%_~.-]*))*";
 
    /**
     * The pattern for a URL.
